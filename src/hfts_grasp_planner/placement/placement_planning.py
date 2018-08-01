Added prototype for post-opt. Changes  so3hierarchy to compute quaternions online. Fixed incorrect computation of quaternions.

src/hfts_grasp_planner/placement/placement_planning.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import os
+import math
 import logging
 import itertools
 import collections
+import functools
 import hfts_grasp_planner.placement.optimization as optimization
 import hfts_grasp_planner.external.transformations as transformations
 import hfts_grasp_planner.placement.so3hierarchy as so3hierarchy
@@ -9,8 +12,6 @@ import hfts_grasp_planner.sdf.kinbody as kinbody_sdf
 import hfts_grasp_planner.utils as utils
 import numpy as np
 import scipy.spatial
-import math
-import os
 import openravepy as orpy
 
 
@@ -434,14 +435,14 @@ class SimplePlacementQuality(object):
         self._body.Enable(False)
         collisions, virtual_contacts = self._env.CheckCollisionRays(rays)
         self._body.Enable(True)
+        distances = np.linalg.norm(tf_plane[1:] - virtual_contacts[:, :3], axis=1)
+        distances[np.invert(collisions)] = np.inf
         ##### DRAW CONTACT ARROWS ###### TODO remove
         handles = []
         for idx, contact in enumerate(virtual_contacts):
-            if collisions[idx]:
+            if collisions[idx] and distances[idx] > 0.001:  # distances shouldn't be too small, otherwise the viewer crashes
                 handles.append(self._env.drawarrow(tf_plane[idx + 1], contact[:3], linewidth=0.002))
         ##### DRAW CONTACT ARROWS - END ######
-        distances = np.linalg.norm(tf_plane[1:] - virtual_contacts[:, :3], axis=1)
-        distances[np.invert(collisions)] = np.inf
         # compute virtual contact plane
         ## first, sort virtual contact points by falling distance
         contact_distance_tuples = zip(distances[collisions], np.where(collisions)[0])
@@ -531,14 +532,15 @@ class SimplePlacementQuality(object):
         return handles
 
 
-class PlacementObjectiveFn(object):
+class PlacementHeuristic(object):
     """
-        Implements an objective function for object placement.
-        #TODO: should this class be in a different module?
+        Implements a heuristic for placement. It consists of two components: a collision cost and a stability cost.
+        The collision cost punishes nodes that are in collision. The stability cost
+        evaluates whether a given node is suitable for releasing an object so that it fall towards a good placement pose.
     """
     def __init__(self, env, scene_sdf, object_base_path, vol_approx=0.005):
         """
-            Creates a new PlacementObjectiveFn
+            Creates a new PlacementHeuristic
             ---------
             Arguments
             ---------
@@ -584,25 +586,35 @@ class PlacementObjectiveFn(object):
         """
         return self._obj_name
 
+    def evaluate_stability(self, pose):
+        """
+            Evalute the stability cost function for the given pose.
+        """
+        self._kinbody.SetTransform(pose)
+        return self._stability_function.compute_quality(pose)
+
+    def evaluate_collision(self, pose):
+        """
+            Evalute the collision cost function for the given pose.
+        """
+        self._kinbody.SetTransform(pose)
+        _, _, col_val, _ = self._kinbody_octree.compute_intersection(self._scene_sdf)
+        return col_val
+
     def evaluate(self, node):
         """
-            Evalutes the objective function for the given node (must be SE3HierarchyNode)
+            Evalutes the objective function for the given node (must be SE3HierarchyNode).
+            The evaluation of the node is performed based on its representative.
         """
         assert(self._kinbody_octree)
         assert(self._scene_sdf)
         # set the transform to the pose to evaluate
         representative = node.get_representative_value()
-        self._kinbody.SetTransform(representative)
         # compute the collision cost for this pose
-        _, _, col_val, _ = self._kinbody_octree.compute_intersection(self._scene_sdf)
-        # if col_val < 0:
-            # return col_val
-        # preference_val = np.dot(representative[:3, 1], np.array([0, 0, 1])) - representative[2, 3]
-        preference_val = self._stability_function.compute_quality(representative)
-        # preference_val = 0.0
-        #TODO return proper objective
-        return preference_val + col_val
-        # return preference_val
+        col_val = self.evaluate_collision(representative)
+        stability_val = self.evaluate_stability(representative)
+        #TODO add weights? different combination of different costs?
+        return stability_val + col_val
 
     @staticmethod
     def is_better(val_1, val_2):
@@ -638,7 +650,6 @@ class SE3Hierarchy(object):
             self._so3_key = so3_key
             self._depth = depth
             self._hierarchy = hierarchy
-            self._so3_hierarchy = hierarchy._so3_hierarchy
             self._cartesian_range = cartesian_box[1] - cartesian_box[0]
             self._child_dimensions = self._cartesian_range / self._hierarchy._cart_branching
             self._child_cache = {}
@@ -658,7 +669,7 @@ class SE3Hierarchy(object):
             """
             if self._depth == self._hierarchy._max_depth:
                 return None
-            bfs = self._so3_hierarchy.get_branching_factors(self._depth)
+            bfs = so3hierarchy.get_branching_factors(self._depth)
             random_child = np.array([np.random.randint(self._hierarchy._cart_branching),
                                      np.random.randint(self._hierarchy._cart_branching),
                                      np.random.randint(self._hierarchy._cart_branching),
@@ -682,7 +693,7 @@ class SE3Hierarchy(object):
             # TODO respect blacklist
             neighbor_id = np.zeros(5, np.int)
             neighbor_id[:3] = np.clip(child_id[:3] + random_dir, 0, max_ids)
-            so3_key = self._so3_hierarchy.get_random_neighbor(node.get_so3_key())
+            so3_key = so3hierarchy.get_random_neighbor(node.get_so3_key())
             neighbor_id[3] = so3_key[0][-1]
             neighbor_id[4] = so3_key[1][-1]
             return self.get_child_node(neighbor_id)
@@ -703,17 +714,23 @@ class SE3Hierarchy(object):
             max_point = min_point + self._child_dimensions
             global_child_id = SE3Hierarchy.construct_id(self._global_id, child_id_key)
             child_node = SE3Hierarchy.SE3HierarchyNode((min_point, max_point),
-                                                        (global_child_id[3], global_child_id[4]),
-                                                        self._depth + 1, self._hierarchy,
-                                                        global_id=global_child_id)
+                                                       (global_child_id[3], global_child_id[4]),
+                                                       self._depth + 1, self._hierarchy,
+                                                       global_id=global_child_id)
             self._child_cache[tuple(child_id_key)] = child_node
             return child_node
+
+        def get_depth(self):
+            """
+                Return the depth of this node.
+            """
+            return self._depth
 
         def get_children(self):
             """
                 Return a generator that allows to iterate over all children.
             """
-            bfs = self._so3_hierarchy.get_branching_factors(self._depth)
+            bfs = so3hierarchy.get_branching_factors(self._depth)
             local_keys = itertools.product(range(self._hierarchy._cart_branching),
                                            range(self._hierarchy._cart_branching),
                                            range(self._hierarchy._cart_branching),
@@ -725,14 +742,20 @@ class SE3Hierarchy(object):
         def get_representative_value(self, rtype=0):
             """
                 Returns a point in SE(3) that represents this cell, i.e. the center of this cell
-                @param rtype - Type to represent point (0 = 4x4 matrix)
+                @param rtype - Type to represent point (0 = 4x4 matrix,
+                                                        1 = [x, y, z, theta, phi, psi] (Hopf coordinates),)
             """
             position = self._cartesian_box[0] + self._cartesian_range / 2.0
-            quaternion = self._so3_hierarchy.get_quaternion(self._so3_key)
             if rtype == 0:  # return a matrix
+                quaternion = so3hierarchy.get_quaternion(self._so3_key)
                 matrix = transformations.quaternion_matrix(quaternion)
                 matrix[:3, 3] = position
                 return matrix
+            elif rtype == 1: # return array with position and hopf coordinates
+                result = np.empty(6)
+                result[:3] = position
+                result[3:] = so3hierarchy.get_hopf_coordinates(self._so3_key)
+                return result
             raise RuntimeError("Return types different from matrix are not implemented yet!")
 
         def get_id(self, relative_to_parent=False):
@@ -747,9 +770,26 @@ class SE3Hierarchy(object):
         def get_so3_key(self):
             return self._so3_key
 
-    """
-        Implements a hierarchical grid on SE3.
-    """
+        def get_bounds(self):
+            """
+                Return workspace range represented by this node. 
+                -------
+                Returns
+                -------
+                bounds, numpy array of shape (6, 2) - each row represents min and max value for [x, y, z, theta, phi, psi]
+            """
+            bounds = np.empty((6, 2))
+            bounds[:3, 0] = self._cartesian_box[0]
+            bounds[:3, 1] = self._cartesian_box[1]
+            bounds[3:, :] = so3hierarchy.get_hopf_coordinate_range(self._so3_key)
+            return bounds
+
+        def is_leaf(self):
+            """
+                Return whether this node is a leaf or not.
+            """
+            return self._depth == self._hierarchy.get_depth()
+
     def __init__(self, bounding_box, cart_branching, depth):
         """
             Creates a new hierarchical grid on SE(3), i.e. R^3 x SO(3)
@@ -759,10 +799,9 @@ class SE3Hierarchy(object):
             @param depth - maximal depth of the hierarchy (can be an integer in {1, 2, 3, 4})
         """
         self._cart_branching = cart_branching
-        self._so3_hierarchy = so3hierarchy.SO3Hierarchy()
-        self._max_depth = max(0, min(depth, self._so3_hierarchy.max_depth()))
+        self._max_depth = depth
         self._root = self.SE3HierarchyNode(cartesian_box=bounding_box,
-                                           so3_key=self._so3_hierarchy.get_root_key(),
+                                           so3_key=so3hierarchy.get_root_key(),
                                            depth=0,
                                            hierarchy=self)
         self._blacklist = None  # TODO need some data structure to blacklist nodes
@@ -799,10 +838,71 @@ class SE3Hierarchy(object):
         return self._max_depth
 
 
+def default_leaf_stage(objective_fn, collision_cost, plcmt_result):
+    """
+        Locally optimize the objective_fn in the domain of the plcmt_result's node
+        using scikit's constrained optimization by linear approximation function.
+        ---------
+        Arguments
+        ---------
+        objective_fn - The function to maximize.
+        collision_cost - Contstraint that needs to be positive.
+    """
+    def to_matrix(x):
+        quat = so3hierarchy.hopf_to_quaternion(x[3:])
+        pose = transformations.quaternion_matrix(quat)
+        pose[:3, 3] = x[:3]
+        return pose
+
+    def pose_wrapper_fn(fn, x, multiplier=1.0):
+        # extract pose from x and pass it to fn
+        val = multiplier * fn(to_matrix(x))
+        if val == float('inf'):
+            val = 10e9  # TODO this is a hack
+        return val
+
+    # get the initial value
+    x0 = plcmt_result.hierarchy_node.get_representative_value(rtype=1)
+    # get bounds
+    bounds = plcmt_result.hierarchy_node.get_bounds()
+    constraints = [
+        {
+            'type': 'ineq',
+            'fun': functools.partial(pose_wrapper_fn, collision_cost),
+        },
+        {
+            'type': 'ineq',
+            'fun': lambda x: x - bounds[:, 0]  # TODO replace with real bounds
+        },
+        {
+            'type': 'ineq',
+            'fun': lambda x: bounds[:, 1] - x  # TODO replace with real bounds
+        }
+    ]
+    opt_result = scipy.optimize.minimize(functools.partial(pose_wrapper_fn, objective_fn, multiplier=-1.0),
+                                         x0, method='COBYLA', constraints=constraints)
+    sol = opt_result.x
+    plcmt_result.obj_pose = to_matrix(sol)
+
+
 class PlacementGoalPlanner:
     """This class allows to search for object placements in combination with
         the FreeSpaceProximitySampler.
     """
+    class PlacementResult(object): # TODO define interface for GoalSampling result
+        """
+            Represents the result of a placement call. 
+            TODO: implement all functions that the goal sampler needs.
+        """
+        def __init__(self, hierarchy_node, quality_value):
+            self.hierarchy_node = hierarchy_node
+            self.quality_value = quality_value
+            self.arm_configuration = None
+            self.obj_pose = hierarchy_node.get_representative_value()
+        
+        def is_leaf(self):
+            return self.hierarchy_node.is_leaf()
+        
     def __init__(self, base_path,
                  env, scene_sdf, visualize=False):
         """
@@ -814,9 +914,13 @@ class PlacementGoalPlanner:
         """
         self._hierarchy = None
         self._env = env
-        self._objective_function = PlacementObjectiveFn(env, scene_sdf, base_path)
-        self._optimizer = optimization.StochasticOptimizer(self._objective_function)
+        self._placement_heuristic = PlacementHeuristic(env, scene_sdf, base_path)
+        self._optimizer = optimization.StochasticOptimizer(self._placement_heuristic)
         # self._optimizer = optimization.StochasticGradientDescent(self._objective_function)
+        # TODO replace the leaf_stage with BayesOpt on Physics?
+        self._leaf_stage = functools.partial(default_leaf_stage,
+                                             self._placement_heuristic.evaluate_stability,
+                                             self._placement_heuristic.evaluate_collision) 
         self._placement_volume = None
         self._env = env
         self._parameters = {'cart_branching': 3, 'max_depth': 4, 'num_iterations': 100}  # TODO update
@@ -828,27 +932,31 @@ class PlacementGoalPlanner:
             @param workspace_volume - (min_point, max_point), where both are np.arrays of length 3
         """
         self._placement_volume = workspace_volume
-        self._objective_function.set_placement_volume(workspace_volume)
+        self._placement_heuristic.set_placement_volume(workspace_volume)
         self._initialized = False
 
     def sample(self, depth_limit, post_opt=True):
         """ Samples a placement configuration from the root level. """
-        #TODO
         if not self._initialized:
             self._initialize()
-        current_node = self.get_root()
-        num_iterations = self._parameters['num_iterations']
-        best_val = None
-        best_node = None
-        for depth in xrange(min(depth_limit, self.get_max_depth())):
-            best_val, best_node = self._optimizer.run(current_node, num_iterations)
-            current_node = best_node
-        return best_node, best_val
+        return self.sample_warm_start(self.get_root(), depth_limit, post_opt=post_opt)
 
     def sample_warm_start(self, hierarchy_node, depth_limit, label_cache=None, post_opt=False):
         """ Samples a placement configuration from the given node on. """
-        #TODO
-        pass
+        if not self._initialized:
+            self._initialize()
+        num_iterations = self._parameters['num_iterations']
+        best_val = None
+        best_node = None
+        current_node = hierarchy_node
+        for depth in xrange(current_node.get_depth(), min(depth_limit, self.get_max_depth())):
+            logging.debug("Searching for placement pose on depth %i" % depth)
+            best_val, best_node = self._optimizer.run(current_node, num_iterations)
+            current_node = best_node
+        placement_result = PlacementGoalPlanner.PlacementResult(best_node, best_val) 
+        if post_opt and placement_result.is_leaf():
+            self._leaf_stage(placement_result)
+        return placement_result
 
     def is_goal(self, sampling_result):
         """ Returns whether the given node is a goal or not. """
@@ -857,6 +965,8 @@ class PlacementGoalPlanner:
 
     def load_hand(self, hand_path, hand_cache_file, hand_config_file, hand_ball_file):
         """ Does nothing. """
+        # TODO The placement model (physics engine) will take the hand into account, so it will have
+        # TODO to be set here.
         pass
 
     def set_object(self, obj_id, model_id=None):
@@ -864,7 +974,7 @@ class PlacementGoalPlanner:
             @param obj_id String identifying the object.
             @param model_id (optional) Name of the model data. If None, it is assumed to be identical to obj_id
         """
-        self._objective_function.set_target_object(obj_id, model_id)
+        self._placement_heuristic.set_target_object(obj_id, model_id)
         self._initialized = False
 
     def set_max_iter(self, iterations):
@@ -886,7 +996,7 @@ class PlacementGoalPlanner:
     def _initialize(self):
         if self._placement_volume is None:
             raise ValueError("Could not intialize as there is no placement volume available")
-        if self._objective_function.get_target_object() is None:
+        if self._placement_heuristic.get_target_object() is None:
             raise ValueError("Could not intialize as there is no placement target object available")
         self._hierarchy = SE3Hierarchy(self._placement_volume,
                                        self._parameters['cart_branching'],  # TODO it makes more sense to provide a resolution instead

src/hfts_grasp_planner/placement/so3hierarchy.py
@@ -1,133 +1,190 @@
 import os
+import healpy
 import numpy as np
 from itertools import izip, product
 
+"""
+    This module contains functions that define a grid hierarchy on SO(3) based on Yershova et al.'s work
+    'Generating Uniform Inccremental Grids on SO(3) using the Hopf Fibration'.
+    This grid has depth 4 (root level + 3 sublevels), and uses Hopf Fibration to represent
+    SO(3). The hierarchy essentially consists of two sub-hierarchies, one for S^2 and one for S^1.
+    As hierarchy on S^2 this hierarchy applies HEALPix; for S^1 it applies just a simple grid on the interval [0,2pi].
+    At the root level, HEALPix has 12 patches, which for each subsequent level in the hierarchy are subdivided by 4.
+    The grid on S^1 has 6 elements on the root level and each element is split by 2 for each subsequent level.
+    Arguments in favor for this hierarchy:
+        - minimizes dispersion and discrepancy on each level of the hierarchy, 
+            i.e. we get a good cover of SO(3) on each level
+        - children of hierarchy nodes are all nearest neighbors of their parents w.r.t. to
+            metric p(x, y) = arccos(|x dot y|), where x and y are quaternions representing
+            rotations
+        - no preference given to what axis is more important. all rotations are treated equally
+            (this makes it look unintuitive when plotting as frames)
+"""
 
-class SO3Hierarchy(object):
+
+S2_ROOT_NEIGHBORS = [[3, 4, 5, 1], [0, 5, 6, 2], [1, 6, 7, 3], [2, 7, 4, 0],
+                        [3, 11, 8, 0], [0, 8, 9, 1], [1, 9, 10, 2], [2, 10, 11, 3],
+                        [4, 11, 5, 9], [8, 5, 6, 10], [9, 6, 7, 11], [10, 7, 4, 8]]
+
+
+def hopf_to_quaternion(hopf):
     """
-        A precomputed grid hierarchy on SO(3) based on Yershova et al.'s work 
-        'Generating Uniform Inccremental Grids on SO(3) using the Hopf Fibration'.
-        This grid has depth 4 (root level + 3 sublevels), and uses Hopf Fibration to represent 
-        SO(3). The hierarchy essentially consists of two sub-hierarchies, one for S^2 and one for S^1.
-        As hierarchy on S^2 this hierarchy applies HEALPix; for S^1 it applies just a simple grid on the interval [0,2pi].
-        At the root level, HEALPix has 12 patches, which for each subsequent level in the hierarchy are subdivided by 4.
-        The grid on S^1 has 6 elements on the root level and each element is split by 2 for each subsequent level.
-        Arguments in favor for this hierarchy:
-            - minimizes dispersion and discrepancy on each level of the hierarchy, 
-               i.e. we get a good cover of SO(3) on each level
-            - children of hierarchy nodes are all nearest neighbors of their parents w.r.t. to
-                metric p(x, y) = arccos(|x dot y|), where x and y are quaternions representing
-                rotations
-            - no preference given to what axis is more important. all rotations are treated equally
-                (this makes it look unintuitive when plotting as frames)
+        Return a quaternion for the given point in Hopf coordinates.
+        ---------
+        Arguments
+        ---------
+        hopf, list or numpy array of length 3 - Hopf coordinates (theta, phi, psi)
+        -------
+        Returns
+        -------
+        quat, numpy array of shape (4,) - quaternion representing orientation of hopf.
     """
-    S2_ROOT_NEIGHBORS = [[3, 4, 5, 1], [0, 5, 6, 2], [1, 6, 7, 3], [2, 7, 4, 0],
-                         [3, 11, 8, 0], [0, 8, 9, 1], [1, 9, 10, 2], [2, 10, 11, 3],
-                         [4, 11, 5, 9], [8, 5, 6, 10], [9, 6, 7, 11], [10, 7, 4, 8]]
-
-    def __init__(self):
-        self._representatives = np.load(os.path.join(os.path.dirname(__file__), 'so3hdata.npy'))
-
-    def get_quaternion(self, key):
-        """
-            Return the quaternion representing the element of the hierarchy with the given key.
-            @param key - The key is expected to be a tuple (S^2_key, S^1_key), where 
-                S^2_key and S^1_key are expected to be lists of positive integers with maximum length 4.
-                In both lists, each integer denotes the local id of a child, e.g. the key [3, 1] represents
-                the first child of the third child on the root level. The integers must be between 0 and the maximum
-                number of children on the respective level, i.e. all S^2_keys must be elementwise smaller than
-                [12, 4, 4, 4] and all S^1 keys must be elementwise smaller than [6, 2, 2, 2].
-            @return quaternion (x1, x2, x3, x4), where x1 is the real part, and x2, x3, x4 are associated with complex
-                parts i,j,k respectively.
-        """
-        idx = self._get_index(key)
-        return self._representatives[idx]
-
-    @staticmethod
-    def max_depth():
-        return 4
-
-    @staticmethod
-    def get_root_key():
-        return ([], [])
-
-    @staticmethod
-    def get_branching_factors(depth):
-        if depth == 0:
-            return (12, 6)
-        return (4, 2)
-
-    @staticmethod
-    def get_random_neighbor(key):
-        # first select a random S^2 neighbor
-        s2_key = list(key[0])
-        s1_key = list(key[1])
-        if len(s2_key) == 0: # just return in case we have an empty key
-            return ()
-        if len(s2_key) == 1: # in case we are on the root level, we have specific choices
-            s2_key[-1] = np.random.choice(SO3Hierarchy.S2_ROOT_NEIGHBORS[s2_key[-1]])
-            rdir = np.random.randint(-1, 2)
-            s1_key[-1] = np.clip(s1_key[-1] + rdir, 0, 5)
-        else:
-            s2_key[-1] = np.random.randint(4)
-            s1_key[-1] = np.random.randint(2)
-        return (s2_key, s1_key)
-
-    @staticmethod
-    def _get_index(key):
-        s2_key, s1_key = key[0], key[1]  # extract keys for both hierarchies
-        level_offset = 0  # we first compute an index offset for the level
-        level = len(s2_key) - 1  # the level of the hierarchy we are on (0 for root)
-        level_offset = 72 * (pow(8, level) - 1) / 7  # this is the number of elements for the previous levels
-        num_s1_values = 6 * pow(2, level)  # number of s1_values on this level for each healpix
-        healpix_idx = 0  # index of this healpix on this level, a value within [0, 12 * 4^level)
-        s1_offset = 0  # offset for s1 indices within a healpix, a value within [0, 6 * 2^level)
-        for d in xrange(len(s2_key)):  # compute offsets
-            healpix_idx += s2_key[d] * pow(4, level - d)  
-            s1_offset += s1_key[d] * pow(2, level - d)
-        within_level_offset = healpix_idx * num_s1_values 
-        return level_offset + within_level_offset + s1_offset
+    quat = np.empty(4)
+    # quat[0] = np.cos(hopf[0] / 2.0) * np.cos(hopf[2] / 2.0)
+    # quat[1] = np.cos(hopf[0] / 2.0) * np.sin(hopf[2] / 2.0)
+    # quat[2] = np.sin(hopf[0] / 2.0) * np.cos(hopf[1] + hopf[2] / 2.0)
+    # quat[3] = np.sin(hopf[0] / 2.0) * np.sin(hopf[1] + hopf[2] / 2.0)
+    quat[0] = np.cos(hopf[0] / 2.0) * np.cos(hopf[2] / 2.0)
+    quat[1] = np.sin(hopf[0] / 2.0) * np.sin(hopf[1] - hopf[2] / 2.0)
+    quat[2] = np.sin(hopf[0] / 2.0) * np.cos(hopf[1] - hopf[2] / 2.0)
+    quat[3] = np.cos(hopf[0] / 2.0) * np.sin(hopf[2] / 2.0)
+    return quat
 
 
-if __name__ == '__main__':
-    # Test indexing
-    hierarchy = SO3Hierarchy()
-    test_array = np.zeros((hierarchy._representatives.shape[0], 1))
-    num_keys = 0
-    print 'Test array shape is ', test_array.shape
-    # root level
-    s2_keys = product(range(12))
-    s1_keys = product(range(6))
-    all_keys = product(s2_keys, s1_keys)
-    for key in all_keys:
-        idx = hierarchy._get_index(key)
-        test_array[idx] += 1
-        num_keys += 1
-    # level 1
-    s2_keys = product(range(12), range(4))
-    s1_keys = product(range(6), range(2))
-    all_keys = product(s2_keys, s1_keys)
-    for key in all_keys:
-        idx = hierarchy._get_index(key)
-        test_array[idx] += 1
-        num_keys += 1
-    # level 2
-    s2_keys = product(range(12), range(4), range(4))
-    s1_keys = product(range(6), range(2), range(2))
-    all_keys = product(s2_keys, s1_keys)
-    for key in all_keys:
-        idx = hierarchy._get_index(key)
-        test_array[idx] += 1
-        num_keys += 1
-    # level 3
-    s2_keys = product(range(12), range(4), range(4), range(4))
-    s1_keys = product(range(6), range(2), range(2), range(2))
-    all_keys = product(s2_keys, s1_keys)
-    for key in all_keys:
-        idx = hierarchy._get_index(key)
-        test_array[idx] += 1
-        num_keys += 1
-    print 'Test array sum is', sum(test_array)
-    print 'Test array min is', min(test_array)
-    print 'Test array max is', max(test_array)
-    print 'Num keys tried is ', num_keys
+def key_to_indices(key):
+    """
+        Translate a hierarchy key to healpix index and index within S1.
+        ---------
+        Arguments
+        ---------
+        key - hierarchy key, see module description.
+        ---------
+        Returns
+        ---------
+        healpix_idx, int - index of the healpix
+        s1_idx, int - index of the inteval in S1
+        level, int - level of the hierarchy (order for healpix)
+    """
+    assert(len(key) == 2)
+    s2_key, s1_key = key[0], key[1]  # extract keys for both hierarchies
+    assert(len(s2_key) == len(s1_key))
+    # compute heal_pix_id
+    level = len(s2_key) - 1  # the level of the hierarchy we are on (0 for root)
+    healpix_idx = 0  # index of this healpix on this level, a value within [0, 12 * 4^level)
+    s1_idx = 0  # s1 index, an integer within [0, 6 * 2^level)
+    for d in xrange(len(s2_key)):  # compute offsets
+        healpix_idx += s2_key[d] * pow(4, level - d)  
+        s1_idx += s1_key[d] * pow(2, level - d)
+    return healpix_idx, s1_idx, level
+
+
+def get_root_key():
+    """
+        Return the key that represents the root (no node) of the hierarchy.
+    """
+    return ([], [])
+
+
+def get_branching_factors(depth):
+    """
+        Return branching factors for the given depth.
+        ---------
+        Arguments
+        ---------
+        depth, int
+        ---------
+        Returns
+        ---------
+        bfs, tuple of int
+    """
+    if depth == 0:
+        return (12, 6)
+    return (4, 2)
+
+
+def get_hopf_coordinates(key):
+    """
+        Return the Hopf coordinates of the element with the given key.
+    """
+    healpix_idx, s1_idx, level = key_to_indices(key)
+    num_s1_values = 6 * pow(2, level)  # number of s1_values on this level for each healpix
+    nside = healpy.order2nside(level)
+    theta, phi = healpy.pix2ang(nside, healpix_idx, nest=True)
+    psi = (s1_idx + 0.5) * 2.0 * np.pi / num_s1_values
+    return np.array([theta, phi, psi])
+
+
+def get_quaternion(key):
+    """
+        Return the quaternion representing the element of the hierarchy with the given key.
+        @param key - The key is expected to be a tuple (S^2_key, S^1_key), where 
+            S^2_key and S^1_key are expected to be lists of positive integers with maximum length 4.
+            In both lists, each integer denotes the local id of a child, e.g. the key [3, 1] represents
+            the first child of the third child on the root level. The integers must be between 0 and the maximum
+            number of children on the respective level, i.e. all S^2_keys must be elementwise smaller than
+            [12, 4, 4, 4] and all S^1 keys must be elementwise smaller than [6, 2, 2, 2].
+        @return quaternion (x1, x2, x3, x4), where x1 is the real part, and x2, x3, x4 are associated with complex
+            parts i,j,k respectively.
+    """
+    hopf_coordinates = get_hopf_coordinates(key)
+    return hopf_to_quaternion(hopf_coordinates)
+
+
+def get_random_neighbor(key):
+    """
+        Return a random neighbor of the element identified by key.
+        ---------
+        Arguments
+        ---------
+        key, tuple - A key identifying a node as described in get_quaternion(..).
+        -------
+        Returns
+        -------
+        neighbor_key, tuple - A key identifying a random neighbor
+    """
+    # first select a random S^2 neighbor
+    s2_key = list(key[0])
+    s1_key = list(key[1])
+    if len(s2_key) == 0: # just return in case we have an empty key
+        return ()
+    if len(s2_key) == 1: # in case we are on the root level, we have specific choices
+        s2_key[-1] = np.random.choice(S2_ROOT_NEIGHBORS[s2_key[-1]])
+        rdir = np.random.randint(-1, 2)
+        s1_key[-1] = np.clip(s1_key[-1] + rdir, 0, 5)
+    else:
+        s2_key[-1] = np.random.randint(4)
+        s1_key[-1] = np.random.randint(2)
+    return (s2_key, s1_key)
+
+
+def get_hopf_coordinate_range(key):
+    """
+        Return the range for the Hopf coordinates that are represented
+        by the element with the given key. TODO Currently it only returns
+        an approximation of this range (a bounding box).
+        ---------
+        Arguments
+        ---------
+        key, tuple - A key identifying a node as described in get_quaternion(..).
+        -------
+        Returns
+        -------
+        range, numpy array of shape (3, 2) - Each row of range represents the min/max values
+            for the respective dimension. The order is theta, phi, psi.
+    """
+    result = np.empty((3, 2))
+    assert(len(key) == 2)
+    s2_key, s1_key = key[0], key[1]  # extract keys for both hierarchies
+    assert(len(s2_key) == len(s1_key))
+    healpix_idx, s1_idx, level = key_to_indices(key)  # get indices
+    num_s1_values = 6 * pow(2, level)  # number of s1_values on this level for each healpix
+    nside = healpy.order2nside(level)  # compute nside for healpix
+    # we approximate the range of theta and phi by computing the corners of the healpix
+    thetas, phis = healpy.vec2ang(healpy.boundaries(nside, healpix_idx, nest=True).transpose())
+    result[0, 0] = np.min(thetas)
+    result[0, 1] = np.max(thetas)
+    result[1, 0] = np.min(phis)
+    result[1, 1] = np.max(phis)
+    result[2, 0] = float(s1_idx) / num_s1_values * np.pi
+    result[2, 1] = (s1_idx + 1.0) / num_s1_values * np.pi
+    return result