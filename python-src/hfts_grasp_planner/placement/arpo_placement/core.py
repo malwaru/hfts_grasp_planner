Added collision constraint gradient to Jacobian descent + bug fixes. Computation of collision constraint gradient too slow.

python-src/hfts_grasp_planner/placement/arpo_placement/core.py
@@ -288,12 +288,12 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
         """
             Struct that stores object data.
             It stores a kinbody, i.e. the object, and a volumetric representation of it -
-            an instance of sdf.kinbody.OccupancyTree
+            an instance of sdf.kinbody.OccupancyTree or sdf.kinbody.RigidBodyOccupancyGrid
         """
 
         def __init__(self, kinbody, occtree):
             self.kinbody = kinbody
-            self.octree = occtree
+            self.volumetric_model = occtree
 
     class RobotData(object):
         """
@@ -307,12 +307,12 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
                 Arguments
                 ---------
                 robot - OpenRAVE robot
-                robot_occtree, sdf.robot.RobotOccupancyTree - volumetric model of the robot
+                robot_volumetric - volumetric model of the robot (either RobotOccupancyGrid or RobotOccupancyOctree)
                 manip_data, dict of ManipulatorData - dict that maps manipulator names to ManipulatorData struct
                 urdf_desc, string - URDF fliename of the robot (content, not the filename!)
             """
             self.robot = robot
-            self.octree = robot_occtree
+            self.volumetric_model = robot_occtree
             self.manip_data = manip_data
             self.urdf_desc = urdf_desc
 
@@ -509,8 +509,8 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
             """
             self._target_obj = object_data.kinbody
             self._manip_data = robot_data.manip_data
-            self._robot_octree = robot_data.octree
-            self._object_octree = object_data.octree
+            self._robot_volumetric = robot_data.volumetric_model
+            self._object_volumetric = object_data.volumetric_model
             self._scene_sdf = scene_sdf
             self._max_robot_intersection = max_robot_intersection
 
@@ -564,19 +564,39 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
                 robot = manip.GetRobot()
                 with robot:
                     robot.SetActiveDOFs(manip.GetArmIndices())
-                    isec_values = self._robot_octree.compute_intersection(
+                    isec_values = self._robot_volumetric.compute_intersection(
                         robot.GetTransform(), arm_config, self._scene_sdf, links=manip_data.manip_links)
                     robot_intersection = isec_values[1]
             # next compute intersection for the object
             obj_tf = np.array(cache_entry.solution.obj_tf)
             # shift the obj tf a bit away from the contact region to ensure we do not count in contact on the surface
-            obj_tf[:3, 3] += cache_entry.region.normal * self._object_octree.get_cell_size()
-            isec_values = self._object_octree.compute_intersection(self._scene_sdf, obj_tf)
+            obj_tf[:3, 3] += cache_entry.region.normal * self._object_volumetric.get_cell_size()
+            isec_values = self._object_volumetric.compute_intersection(self._scene_sdf, obj_tf)
             # from this compute relaxation value that is in interval 0, 1
             object_intersection = isec_values[1]
             violation_term = np.clip((object_intersection + robot_intersection /
                                       self._max_robot_intersection)/2.0, 0.0, 1.0)
             return 1.0 - violation_term
+
+        def get_chomps_collision_gradient(self, cache_entry, config):
+            """
+                Return the gradient of CHOMP's collision cost.
+                NOTE: For this to work correctly, the active DOFs of the robot must be set before.
+                ---------
+                Arguments
+                ---------
+                cache_entry, SolutionCacheEntry
+                config, np array of shape (q,) - robot configuration with q = active DOFs elements
+                -------
+                Returns
+                -------
+                gradient, np array of shape (q,) - gradient of CHOMP's collision cost
+            """
+            manip = cache_entry.solution.manip
+            manip_data = self._manip_data[manip.GetName()]
+            _, gradient = self._robot_volumetric.compute_penetration_cost(
+                self._scene_sdf, config, b_compute_gradient=True, links=manip_data.manip_links)
+            return gradient
 
     class ReachabilityConstraint(object):
         """
@@ -741,6 +761,7 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
             self.manip_data = robot_data.manip_data
             self.epsilon = 0.01  # minimal magnitude of cspace gradient
             self.step_size = 0.01  # multiplier for update step
+            self.max_iterations = 50  # maximal number of iterations
             self.robot = robot_data.robot
 
         def optimize(self, cache_entry):
@@ -758,6 +779,7 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
                 cache_entry.solution.arm_config, numpy array of shape (n,) - arm configuration (n DOFs)
             """
             rospy.logdebug("Running JacobianOptimizer to make solution feasible")
+            q_original = self.robot.GetActiveDOFValues()  # TODO remove
             manip_data = self.manip_data[cache_entry.solution.manip.GetName()]
             lower, upper = manip_data.lower_limits, manip_data.upper_limits
             manip = manip_data.manip
@@ -776,7 +798,10 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
                 grad_norm = 0.0
             b_in_limits = (q_current >= lower).all() and (q_current <= upper).all()
             # iterate as long as the gradient is not zero and we are not beyond limits
-            while q_grad is not None and grad_norm > self.epsilon and b_in_limits:
+            # while q_grad is not None and grad_norm > self.epsilon and b_in_limits:
+            for i in xrange(self.max_iterations):
+                if q_grad is None or grad_norm == 0.0 or not b_in_limits:
+                    break
                 rospy.logdebug("Updating q_current %s in direction of gradient %s (magnitude %f)" %
                                (str(q_current), str(q_grad), grad_norm))
                 q_current -= self.step_size * q_grad / grad_norm
@@ -791,9 +816,11 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
                         grad_norm = 0.0
                 else:
                     rospy.logdebug("Jacobian descent has led to joint limit violation. Aborting")
-            rospy.logdebug("Jacobian descent finished")
+                # iteration += 1
+            rospy.logdebug("Jacobian descent finished after %i iterations" % i)
             cache_entry.solution.arm_config = q_best
             manip.SetLocalToolTransform(np.eye(4))
+            self.robot.SetActiveDOFValues(q_original)  # TODO remove
 
         def _compute_gradient(self, cache_entry, q_current, manip_data):
             """
@@ -807,48 +834,63 @@ class ARPORobotBridge(placement_interfaces.PlacementSolutionConstructor,
             """
             manip = manip_data.manip
             jacobian = np.empty((6, manip.GetArmDOF()))
-            with self.robot:
-                # compute jacobian
-                self.robot.SetActiveDOFValues(q_current)
-                # TODO delete
-                # crayola = self.robot.GetEnv().GetKinBody("crayola")
-                # crayola.SetTransform(
-                #     np.matmul(cache_entry.solution.manip.GetEndEffector().GetTransform(), manip_data.inv_grasp_tf))
-                # TODO until here
-                jacobian[:3] = manip.CalculateJacobian()
-                jacobian[3:] = manip.CalculateAngularVelocityJacobian()
-                # compute pseudo inverse
-                # inv_jac, rank = utils.compute_pseudo_inverse_rank(jacobian)
-                # TODO see what is faster, using above numpy implementation or this
-                rank = np.linalg.matrix_rank(jacobian)
-                if rank < 6:  # if we are in a singularity, just return None
-                    rospy.logdebug("Jacboian descent failed: Ran into singularity.")
-                    return None
-                inv_jac = np.linalg.pinv(jacobian)
-                # Compute gradient w.r.t. constraints
-                # get pose of placement reference point
-                ref_pose = manip.GetEndEffectorTransform()  # this takes the local tool transform into account
-                # 1. Region constraint - reference point needs to be within the selected placement region
-                region = cache_entry.region
-                b_valid, region_pos_grad = region.aabb_dist_gradient_field.get_interpolated_vectors(
-                    ref_pose[:3, 3].reshape((1, 3)))
-                if not b_valid[0]:
-                    rospy.logdebug("Jacobian descent failed: It went out of the placement region's aabb.")
-                    return None
-                cart_grad_r = np.array([region_pos_grad[0, 0], region_pos_grad[0, 1], 0.0])
-                rospy.logdebug("In-region constraint gradient is %s" % str(cart_grad_r))
-                # TODO check whether theta is out of range, and return None in that case
-                # 2. Stability - all contact points need to be in a placement region (any)
-                cart_grad_c = self.contact_constraint.compute_cart_gradient(cache_entry, ref_pose)
-                rospy.logdebug("Contact constraint gradient is %s" % str(cart_grad_c))
-                cart_grad = cart_grad_r + cart_grad_c
-                # Translate cart_grad to q_grad using javobian inverse
-                qgrad = np.matmul(inv_jac, np.array([cart_grad[0], cart_grad[1], 0.0, 0.0, 0.0, cart_grad[2]]))
-                # TODO compute gradient based on collisions
-                # remove any motion that changes the base orientation/z height of the object
-                jacobian[[0, 1, 5], :] = 0.0  # motion in x, y, ez is allowed
-                qgrad = np.matmul((np.eye(qgrad.shape[0]) - np.matmul(inv_jac, jacobian)), qgrad)
-                return qgrad
+            # with self.robot:
+            # compute jacobian
+            self.robot.SetActiveDOFValues(q_current)
+            # TODO delete
+            crayola = self.robot.GetEnv().GetKinBody("crayola")
+            crayola.SetTransform(
+                np.matmul(cache_entry.solution.manip.GetEndEffector().GetTransform(), manip_data.inv_grasp_tf))
+            # TODO until here
+            jacobian[:3] = manip.CalculateJacobian()
+            jacobian[3:] = manip.CalculateAngularVelocityJacobian()
+            # compute pseudo inverse
+            # inv_jac, rank = utils.compute_pseudo_inverse_rank(jacobian)
+            # TODO see what is faster, using above numpy implementation or this
+            # rank = np.linalg.matrix_rank(jacobian)
+            # if rank < 6:  # if we are in a singularity, just return None
+            #     rospy.logdebug("Jacboian descent failed: Ran into singularity.")
+            #     return None
+            # inv_jac = np.linalg.pinv(jacobian)
+            # Compute gradient w.r.t. constraints
+            # get pose of placement reference point
+            ref_pose = manip.GetEndEffectorTransform()  # this takes the local tool transform into account
+            # ----- 1. Region constraint - reference point needs to be within the selected placement region
+            region = cache_entry.region
+            b_valid, region_pos_grad = region.aabb_dist_gradient_field.get_interpolated_vectors(
+                ref_pose[:3, 3].reshape((1, 3)))
+            if not b_valid[0]:
+                rospy.logdebug("Jacobian descent failed: It went out of the placement region's aabb.")
+                return None
+            cart_grad_r = np.array([region_pos_grad[0, 0], region_pos_grad[0, 1], 0.0])
+            rospy.logdebug("In-region constraint gradient is %s" % str(cart_grad_r))
+            # ----- 2. Theta constraint - theta needs to be within the selected so2interval
+            # We compute no gradient for this constraint, and instead simply abort if we violate it
+            relative_tf = np.matmul(utils.inverse_transform(cache_entry.region.base_tf), ref_pose)
+            ex, ey, ez = tf_mod.euler_from_matrix(relative_tf)
+            theta = utils.normalize_radian(ez)
+            rospy.logdebug("Current [x=%f, y=%f, z=%f, ex=%f, ey=%f, ez=%f, theta=%f]" %
+                           (relative_tf[0, 3], relative_tf[1, 3], relative_tf[2, 3], ex, ey, ez, theta))
+            if utils.dist_in_range(theta, cache_entry.so2_interval) != 0.0:
+                rospy.logdebug("Jacobian descent failed: Theta is out of so2 interval.")
+                return None
+            # ------ 3. Stability - all contact points need to be in a placement region (any)
+            cart_grad_c = self.contact_constraint.compute_cart_gradient(cache_entry, ref_pose)
+            rospy.logdebug("Contact constraint gradient is %s" % str(cart_grad_c))
+            cart_grad = cart_grad_r + cart_grad_c
+            rospy.logdebug("Resulting cartesian gradient (x, y, theta): %s" % str(cart_grad))
+            sub_jacobian = np.array([jacobian[0], jacobian[1], jacobian[5]])
+            qgrad = np.matmul(cart_grad, sub_jacobian)
+            # ------ 4. Collision constraint - object must not be in collision
+            # TODO implement me
+            # ------ 5. Collision constraint - arm must not be in collision
+            col_grad = self.collision_constraint.get_chomps_collision_gradient(cache_entry, q_current)
+            qgrad += col_grad
+            # remove any motion that changes the base orientation/z height of the object
+            inv_jac = np.linalg.pinv(jacobian)
+            jacobian[[0, 1, 5], :] = 0.0  # motion in x, y, ez is allowed
+            qgrad = np.matmul((np.eye(qgrad.shape[0]) - np.matmul(inv_jac, jacobian)), qgrad)
+            return qgrad
 
     def __init__(self, arpo_hierarchy, robot_data, object_data,
                  objective_fn, global_region_info, scene_sdf):

python-src/hfts_grasp_planner/sdf/core.py
@@ -14,7 +14,7 @@ import numpy as np
 import openravepy as orpy
 from itertools import izip, product
 import scipy.ndimage.morphology
-from hfts_grasp_planner.sdf.grid import VoxelGrid
+from hfts_grasp_planner.sdf.grid import VoxelGrid, VectorGrid
 from hfts_grasp_planner.sdf.occupancy import OccupancyGridBuilder
 from hfts_grasp_planner.sdf.visualization import ORVoxelGridVisualization
 # from mayavi import mlab
@@ -113,6 +113,26 @@ class SDF(object):
         else:
             distances = map(self._get_heuristic_distance_local, local_points[:, :3])
         return distances
+
+    def get_distances_grad(self, positions):
+        """
+            Return the shortest distance to obstacles at the given points as well as the gradients.
+            This method interpolates distances between grid points.
+            TODO: If any query position is out of bounds, it currently throws an exception.
+            ---------
+            Arguments
+            ---------
+            positions, a numpy array of shape (n, 3) - n query positions in global frame
+            -------
+            Returns
+            -------
+            values, np array of shape (n,) - distances at the query positions
+            gradients, np array of shape (n, 3) - gradients at the query positions
+        """
+        valid_mask, values, gradients = self._grid.get_cell_gradients_pos(positions)
+        if np.sum(valid_mask) < values.shape[0]:
+            raise NotImplementedError("Dealing with positions that are out of bounds is not implemented yet")
+        return values, gradients
 
     def get_direction(self, point):
         """
@@ -232,6 +252,26 @@ class SDFBuilder(object):
         """
         self._cell_size = cell_size
         self._occupancy_builder = OccupancyGridBuilder(env, cell_size)
+
+    @staticmethod
+    def compute_gradient_field(sdf):
+        """
+            Compute the gradient field of the given sdf.
+            ---------
+            Arguments
+            ---------
+            sdf, SDF to compute gradients for
+            -------
+            Returns
+            -------
+            gradf, VectorGrid
+        """
+        cell_size = sdf._grid.get_cell_size()
+        raw_data = sdf._grid.get_raw_data()
+        vector_field = VectorGrid(sdf._grid.get_aabb(), cell_size=cell_size,
+                                  base_transform=sdf._grid.get_transform(), num_cells=np.array(raw_data.shape))
+        vector_field.vectors[0], vector_field.vectors[1], vector_field.vectors[2] = np.gradient(raw_data, cell_size)
+        return vector_field
 
     @staticmethod
     def compute_sdf(grid, b_compute_dirs=False):
@@ -479,6 +519,36 @@ class SceneSDF(object):
         if self._static_sdf is not None:
             min_distances = np.minimum(min_distances, self._static_sdf.get_distances(positions))
         return min_distances
+
+    def get_distances_grad(self, positions):
+        """
+            Returns the signed distance from the given position to the closest obstacle surface
+            as well as the gradient of that distance.
+            TODO: raises NotImplementedError if any position is out of bounds - this also holds for movables!
+            ---------
+            Arguments
+            ---------
+            positions, np array of shape (n, 3) - n query positions
+            -------
+            Returns
+            -------
+            distances, np array of shape (n,) - distances
+            gradients, np.array of shape (n, 3) - gradients
+        """
+        min_distances = np.full(positions.shape[0], float('inf'))
+        gradients = np.zeros((positions.shape[0], 3))
+        for (body, body_sdf) in self._body_sdfs.itervalues():
+            body_sdf.set_transform(body.GetTransform())
+            distances, bgradients = body_sdf.get_distances_grad(positions)
+            selection_mask = distances < min_distances
+            gradients[selection_mask] = bgradients[selection_mask]
+            min_distances = np.min((distances, min_distances), axis=0)
+        if self._static_sdf is not None:
+            distances, sgradients = self._static_sdf.get_distances_grad(positions)
+            selection_mask = distances < min_distances
+            gradients[selection_mask] = sgradients[selection_mask]
+            min_distances = np.min((distances, min_distances), axis=0)
+        return min_distances, gradients
 
     def get_direction(self, position):
         """

python-src/hfts_grasp_planner/sdf/kinbody.py
@@ -477,6 +477,7 @@ class RigidBodyOccupancyGrid(object):
             self._grid = construct_grid([link], cell_size)[0]
         else:
             self._grid = grid
+            cell_size = grid.get_cell_size()
         cells = self._grid.get_raw_data()
         xx, yy, zz = np.nonzero(cells)
         indices_mat = np.column_stack((xx, yy, zz))
@@ -525,7 +526,7 @@ class RigidBodyOccupancyGrid(object):
             return np.count_nonzero(comp(field.get_cell_values(indices), val))
         return 0
 
-    def intersect(self, scene_sdf, tf=None):
+    def compute_intersection(self, scene_sdf, tf=None):
         """
             Return how many cells of this occupancy grid correspond to positions
             with negative or zero distance in the given scene_sdf.
@@ -549,6 +550,58 @@ class RigidBodyOccupancyGrid(object):
         num_intersecting_cells = np.sum(distances <= 0.0)
         return num_intersecting_cells, float(num_intersecting_cells) / self._num_occupied_cells, num_intersecting_cells * self._cell_volume
 
+    def compute_obstacle_cost(self, scene_sdf, tf=None, bgradients=False, eps=None):
+        """
+            Compute obstacle penetration cost from CHOMP paper C(x) = sum_i(c(x_i)) where x_i are the voxel positions
+            and:
+                    -d(x) + eps / 2             if d(x) < 0.0
+            c(x) =  1/(2eps)(d(x) - eps)^2      if 0 <= d(x) <= eps
+                    0                           else
+            with d(x) being the minimal distance to an obstacle (i.e. what is given by scene_sdf)
+            ---------
+            Arguments
+            ---------
+            scene_sdf, SceneSDF - to retrieve d(x)
+            tf(optional), np array of shape (4, 4) - link pose, if not provided use link's current pose
+            bgradients(optional), bool - if true, also return gradients of c(x_i) w.r.t x_i (one for each voxel)
+            eps(optional), float - eps in above formula = minimal distance a collision-free link should have.
+                If not provided cell size of underlying occupancy grid is used.
+            -------
+            Returns
+            -------
+            value, float - intersection cost
+            gradients(optional), numpy array of shape (n, 3) - gradients dc(x_i)/dx_i if bgradients is True
+            local_positions(optional), np array of shape (n, 3) - positions of voxel positions in link frame if bgradients is True
+        """
+        if tf is None:
+            tf = self._link.GetTransform()
+        if eps is None:
+            eps = self._grid.get_cell_size()
+        query_pos = np.dot(self._locc_positions, tf[:3, :3].transpose()) + tf[:3, 3]
+        if bgradients:
+            distances, dist_gradients = scene_sdf.get_distances_grad(query_pos)
+        else:
+            distances = scene_sdf.get_distances(query_pos)
+        return_values = np.empty_like(distances)
+        # compute array access masks to distuingish between cases
+        negative_mask = distances < 0.0
+        buffer_mask = np.logical_and(distances < eps, np.logical_not(negative_mask))
+        negative_ind = np.nonzero(negative_mask)[0]
+        buffer_ind = np.nonzero(buffer_mask)[0]
+        positive_ind = np.nonzero(distances > eps)[0]
+        # compute the actual funciton
+        return_values[negative_ind] = - distances[negative_ind] + eps / 2.0
+        return_values[buffer_ind] = 1.0 / (2.0 * eps) * np.square((distances[buffer_ind] - eps))
+        return_values[positive_ind] = 0.0
+        # optionally compute gradients
+        if bgradients:
+            gradients = np.empty_like(dist_gradients)
+            gradients[negative_ind] = -dist_gradients[negative_ind]
+            gradients[buffer_ind] = (dist_gradients[buffer_ind].T * (distances[buffer_ind] - eps) * 1.0/eps).T
+            gradients[positive_ind] = 0.0
+            return return_values, gradients, self._locc_positions
+        return return_values
+
     def get_link(self):
         """
             Return the link this grid represents.
@@ -566,6 +619,12 @@ class RigidBodyOccupancyGrid(object):
             Return the number of cells in the underlying grid that are occupied.
         """
         return self._num_occupied_cells
+
+    def get_cell_size(self):
+        """
+            Return cell size of underlying grid.
+        """
+        return self._grid.get_cell_size()
 
     def save(self, filename):
         """
@@ -704,7 +763,7 @@ if __name__ == '__main__':
         scene_sdf = sdf_core.SceneSDF(env, [], [\'Yumi\', \'crayola\']);\
         scene_sdf.load(base_path + \'data/sdfs/placement_exp_0.sdf\');\
         """
-        run_code = """occupancy_grid.intersect(scene_sdf);"""
+        run_code = """occupancy_grid.compute_intersection(scene_sdf);"""
         import timeit
         num_runs = 10000
         total = timeit.timeit(run_code, setup=setup_code, number=num_runs)

python-src/hfts_grasp_planner/sdf/robot.py
@@ -43,11 +43,13 @@ class RobotOccupancyGrid(object):
             link_names = all_link_names
         links = [robot.GetLink(name) for name in link_names]
         if occupancy_grids is None:
-            self._occupancy_grids = RigidBodyOccupancyGrid.create_occupancy_grids(cell_size, links)
+            grid_list = RigidBodyOccupancyGrid.create_occupancy_grids(cell_size, links)
         else:
-            self._occupancy_grids = occupancy_grids
-        self._total_volume = sum([grid.get_volume() for grid in self._occupancy_grids])
-        self._total_num_occupied_cells = sum([grid.get_num_occupied_cells() for grid in self._occupancy_grids])
+            grid_list = occupancy_grids
+        self._occupancy_grids = {grid.get_link().GetName(): grid for grid in grid_list}
+        self._total_volume = sum([grid.get_volume() for grid in self._occupancy_grids.itervalues()])
+        self._total_num_occupied_cells = sum([grid.get_num_occupied_cells()
+                                              for grid in self._occupancy_grids.itervalues()])
 
     @staticmethod
     def load(base_file_name, robot, link_names=None):
@@ -86,7 +88,7 @@ class RobotOccupancyGrid(object):
         for link_name, grid in self._occupancy_grids.iteritems():
             grid.save(base_file_name + '.' + link_name)
 
-    def compute_intersection(self, robot_pose, robot_config, scene_sdf):
+    def compute_intersection(self, robot_pose, robot_config, scene_sdf, links=None):
         """
             Computes the intersection between the occupancy grids of the robot's links
             and the geometry in the scene described by the provided scene sdf.
@@ -96,6 +98,8 @@ class RobotOccupancyGrid(object):
             robot_pose, numpy array of shape (4, 4) - pose of the robot
             robot_config, numpy array of shape (q,) - configuration of active DOFs
             scene_sdf, signed distance field
+            links (optional), list of OpenRAVE links - if provided, limits intersection computation
+                to the given links
             -------
             Returns
             -------
@@ -104,13 +108,64 @@ class RobotOccupancyGrid(object):
                 rv is this volume relative to the robot's total volume, i.e. in range [0, 1]
         """
         with self._robot:
+            if links is None:
+                grids = self._occupancy_grids.values()
+            else:
+                grids = [self._occupancy_grids[link.GetName()]
+                         for link in links if link.GetName() in self._occupancy_grids]
             self._robot.SetTransform(robot_pose)
             self._robot.SetActiveDOFValues(robot_config)
             v = 0.0
-            for grid in self._occupancy_grids:
-                _, _, lv, = grid.intersect(scene_sdf)
+            for grid in grids:
+                _, _, lv, = grid.compute_intersection(scene_sdf)
                 v += lv
             return v, v / self._total_volume
+
+    def compute_penetration_cost(self, scene_sdf, robot_config, b_compute_gradient=False, robot_pose=None, links=None):
+        """
+            Compute CHOMP's penetration cost for the robot.
+            ---------
+            Arguments
+            ---------
+            scene_sdf, SceneSDF - scene sdf to retrieve distances from
+            robot_config, np array of shape (q,) - configuration of active DOFs
+            b_compute_gradients(optional), bool - if True, also compute gradient of the cost w.r.t. active DOFs
+            robot_pose(optional), np array of shape (4, 4) - robot pose (default current)
+            links (optional), list of OpenRAVE links - if provided, limits computation to the given links
+            -------
+            Returns
+            -------
+            penetration_cost, float - penetration cost of the robot (in range [0, inf))
+            gradient(optional), np array of shape (q,) - If requested, gradient w.r.t active DOFs
+        """
+        with self._robot:
+            if robot_pose is not None:
+                self._robot.SetTransform(robot_pose)
+            self._robot.SetActiveDOFValues(robot_config)
+            if links is None:
+                grids = self._occupancy_grids.values()
+            else:
+                grids = [self._occupancy_grids[link.GetName()]
+                         for link in links if link.GetName() in self._occupancy_grids]
+            if b_compute_gradient:
+                total_value = 0.0
+                gradient = np.zeros(self._robot.GetActiveDOF())
+                for grid in grids:
+                    link_id = grid.get_link().GetIndex()
+                    values, cart_gradients, lposs = grid.compute_obstacle_cost(
+                        scene_sdf, bgradients=b_compute_gradient)
+                    total_value += np.sum(values)
+                    for cart_grad, lpos in izip(cart_gradients, lposs):
+                        jacobian = self._robot.CalculateActiveJacobian(link_id, lpos)
+                        gradient += np.matmul(cart_grad, jacobian)  # it's jacobian.T * cart_grad
+                return total_value, gradient
+            else:
+                total_value = 0.0
+                for grid in grids:
+                    link_id = grid.get_link().GetIndex()
+                    values = grid.compute_obstacle_cost(scene_sdf, bgradients=b_compute_gradient)
+                    total_value += np.sum(values)
+                return total_value
 
     def get_robot(self):
         """

scripts/test_scripts/test_placement2.py
@@ -6,6 +6,7 @@ import rospy
 import time
 import argparse
 import numpy as np
+import cProfile
 import openravepy as orpy
 # import hfts_grasp_planner.placement
 from hfts_grasp_planner.utils import is_dynamic_body, inverse_transform, get_manipulator_links
@@ -53,7 +54,7 @@ def resolve_paths(problem_desc, yaml_file):
         global_yaml = cwd + '/' + global_yaml
     head, _ = os.path.split(global_yaml)
     for key in ['or_env', 'occ_file', 'sdf_file', 'urdf_file', 'data_path', 'gripper_file', 'grasp_file',
-                'robot_occtree', 'reachability_path']:
+                'robot_occtree', 'robot_occgrid', 'reachability_path']:
         if key in problem_desc:
             problem_desc[key] = os.path.normpath(head + '/' + problem_desc[key])
 
@@ -222,21 +223,31 @@ if __name__ == "__main__":
         # build robot_octree
         try:
             now = time.time()
-            robot_octree = robot_sdf_module.RobotOccupancyOctree.load(base_file_name=problem_desc['robot_occtree'],
-                                                                      robot=robot, link_names=link_names)
-            rospy.logdebug("Loading robot octree took %fs" % (time.time() - now))
+            # robot_octree = robot_sdf_module.RobotOccupancyOctree.load(base_file_name=problem_desc['robot_occtree'],
+            #                                                           robot=robot, link_names=link_names)
+            # rospy.logdebug("Loading robot octree took %fs" % (time.time() - now))
+            robot_occgrid = robot_sdf_module.RobotOccupancyGrid.load(base_file_name=problem_desc['robot_occgrid'],
+                                                                     robot=robot, link_names=link_names)
+            rospy.logdebug("Loading robot occgrid took %fs" % (time.time() - now))
         except IOError:
-            robot_octree = robot_sdf_module.RobotOccupancyOctree(
-                problem_desc['parameters']['occ_tree_cell_size'], robot, link_names)
-            robot_octree.save(problem_desc['robot_occtree'])
+            # robot_octree = robot_sdf_module.RobotOccupancyOctree(
+            #     problem_desc['parameters']['occ_tree_cell_size'], robot, link_names)
+            # robot_octree.save(problem_desc['robot_occtree'])
+            now = time.time()
+            robot_occgrid = robot_sdf_module.RobotOccupancyGrid(problem_desc['parameters']['occ_tree_cell_size'],
+                                                                robot=robot, link_names=link_names)
+            robot_occgrid.save(problem_desc['robot_occgrid'])
+            rospy.logdebug("Creating robot occgrid took %fs" % (time.time() - now))
         urdf_content = None
         with open(problem_desc['urdf_file'], 'r') as urdf_file:
             urdf_content = urdf_file.read()
-        robot_data = arpo_placement_mod.ARPORobotBridge.RobotData(robot, robot_octree, manip_data, urdf_content)
+        robot_data = arpo_placement_mod.ARPORobotBridge.RobotData(robot, robot_occgrid, manip_data, urdf_content)
         # create object data
-        obj_octree = kinbody_sdf_module.OccupancyOctree(
-            problem_desc['parameters']['occ_tree_cell_size'], target_object.GetLinks()[0])
-        object_data = arpo_placement_mod.ARPORobotBridge.ObjectData(target_object, obj_octree)
+        # obj_octree = kinbody_sdf_module.OccupancyOctree(
+        #     problem_desc['parameters']['occ_tree_cell_size'], target_object.GetLinks()[0])
+        obj_occgrid = kinbody_sdf_module.RigidBodyOccupancyGrid(problem_desc['parameters']['occ_tree_cell_size'],
+                                                                target_object.GetLinks()[0])
+        object_data = arpo_placement_mod.ARPORobotBridge.ObjectData(target_object, obj_occgrid)
         # create arpo hierarchy
         hierarchy = arpo_placement_mod.ARPOHierarchy(manips, regions, orientations, so2_depth=4, so2_branching=4)
         arpo_bridge = arpo_placement_mod.ARPORobotBridge(arpo_hierarchy=hierarchy, robot_data=robot_data,
@@ -248,7 +259,7 @@ if __name__ == "__main__":
         # handles.append(draw_volume(env, placement_volume))
         # handles = visualize_occupancy_grid(env, surface_grid, color=np.array([1.0, 0.0, 0.0, 0.2]))
         handles.extend(plcmnt_regions_mod.visualize_plcmnt_regions(
-            env, regions, height=occ_target_volume.get_cell_size(), level=1))
+            env, regions, height=occ_target_volume.get_cell_size(), level=2))
         # print "Check the placement regions!"
         # IPython.embed()
         # handles = []
@@ -267,6 +278,9 @@ if __name__ == "__main__":
         # solutions, num_solutions = goal_sampler.sample(1, 1)
         # probe = env.GetKinBody("probe")
         # prober = kinbody_sdf_module.RigidBodyOccupancyGrid(0.005, probe.GetLinks()[0])
+        # rospy.loginfo("Starting cProfile")
+        # cProfile.run("goal_sampler.sample(2, 10)", '/tmp/cprofile_placement')
+        # rospy.loginfo("cProfile complete")
         IPython.embed()
     finally:
         orpy.RaveDestroy()