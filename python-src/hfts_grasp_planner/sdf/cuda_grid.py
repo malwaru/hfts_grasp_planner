Bugfix in cuda-based gradient computation

python-src/hfts_grasp_planner/sdf/cuda_grid.py
@@ -120,7 +120,7 @@ class CudaInterpolator(object):
             self._get_val_fn = self._mod.get_function("get_val_layered")
             self._get_val_fn.prepare(("P", "P", "P", np.float32, np.int32), texrefs=[self._tex_field])
             self._get_val_grad_fn = self._mod.get_function("get_val_and_grad_layered")
-            self._get_val_grad_fn.prepare(("P", "P", "P", "P", np.float32, np.float32, np.int32),
+            self._get_val_grad_fn.prepare(("P", "P", "P", "P", "P", np.float32, np.float32, np.int32),
                                           texrefs=[self._tex_field])
         else:
             # allocate data to texture memory
@@ -131,10 +131,11 @@ class CudaInterpolator(object):
             self._get_val_fn = self._mod.get_function("get_val")
             self._get_val_fn.prepare(("P", "P", "P", np.float32, np.int32), texrefs=[self._tex_field])
             self._get_val_grad_fn = self._mod.get_function("get_val_and_grad")
-            self._get_val_grad_fn.prepare(("P", "P", "P", "P", np.float32, np.float32, np.int32),
+            self._get_val_grad_fn.prepare(("P", "P", "P", "P", "P", np.float32, np.float32, np.int32),
                                           texrefs=[self._tex_field])
-        # allocate memory for transformation matrix
-        self._gpu_tf_matrix = cuda.mem_alloc(48)
+        # allocate memory for transformation matrix and gradient directions
+        self._gpu_tf_matrix = cuda.mem_alloc(12 * 4)  # 12 float32
+        self._gpu_grad_dirs = cuda.mem_alloc(9 * 4)  # 9 float32
         # init variables for dynamic memory
         self._gpu_values = None
         self._gpu_grads = None
@@ -230,18 +231,21 @@ class CudaInterpolator(object):
             positions = positions.astype(np.float32)
         if tf_matrix.dtype != np.float32:
             tf_matrix = tf_matrix.astype(np.float32)
+        gradient_dirs = tf_matrix[:3, :3].transpose().flatten()
         tf_matrix = tf_matrix.flatten()[:12]
         self._allocate_gpu_mem(num_pos, True)
         # copy positions to gpu
         cuda.memcpy_htod(self._gpu_positions, positions)
         # copy tf matrix to gpu
         cuda.memcpy_htod(self._gpu_tf_matrix, tf_matrix)
+        # copy gradient directions to gpu
+        cuda.memcpy_htod(self._gpu_grad_dirs, gradient_dirs)
         # compute number of threads
         grid = (int(math.ceil(num_pos / 1024.0)), 1)  # there can be at most 1024 threads per block
         block = (min(1024, num_pos), 1, 1)
         self._get_val_grad_fn.prepared_call(grid, block,
-                                       self._gpu_positions, self._gpu_values, self._gpu_grads, self._gpu_tf_matrix,
-                                       np.float32(scale), np.float32(delta), np.int32(num_pos))
+                                       self._gpu_positions, self._gpu_values, self._gpu_grads, self._gpu_grad_dirs,
+                                        self._gpu_tf_matrix, np.float32(scale), np.float32(delta), np.int32(num_pos))
         # copy result
         cuda.memcpy_dtoh(self._cpu_values[:num_pos], self._gpu_values)
         cuda.memcpy_dtoh(self._cpu_grads[:num_pos], self._gpu_grads)
@@ -267,7 +271,9 @@ class CudaStaticPositionsInterpolator(object):
             data, np.array of shape (n, m, k), float - grid data
             positions, np array of shape (j, 3), float - positions to perform queries for
             base_tf, np array of shape (4, 4), float - base transformation matrix that input tf matrices should be left
-                multiplied by
+                multiplied by (map from world frame to data frame). This also defines the frame the gradients are computed for.
+                The gradients will computed along the column vectors of base_tf, i.e. if base_tf maps from frame A to the
+                local grid frame, the gradients will be computed w.r.t. frame A's x, y, and z axis.
             scale, float - scaling factor from positions to grid indices
             delta, float - step size for numerical gradient computation
         """
@@ -300,15 +306,19 @@ class CudaStaticPositionsInterpolator(object):
         # allocate memory for gradients
         self._gpu_gradients = cuda.mem_alloc(positions.nbytes)
         self._cpu_gradients = np.empty((num_pos, 3), dtype=np.float32)
-        # allocate memory for matrix
-        self._gpu_tf_matrix = cuda.mem_alloc(48)
+        # allocate memory for matrix and gradient dirs
+        self._gpu_tf_matrix = cuda.mem_alloc(12 * 4)  # 12 float32
+        self._gpu_grad_dirs = cuda.mem_alloc(9 * 4)  # 9 float32
+        # copy gradient directions
+        float32_base_tf = base_tf.astype(np.float32)
+        cuda.memcpy_htod(self._gpu_grad_dirs, float32_base_tf[:3, :3].transpose().flatten())
         # get actual functions and prepare them
         self._get_val_fn = self._mod.get_function("get_val")
         self._get_val_fn.prepare(("P", "P", "P", np.float32, np.int32), texrefs=[self._tex_field])
         self._get_val_grad_fn = self._mod.get_function("get_val_and_grad")
-        self._get_val_grad_fn.prepare(("P", "P", "P", "P", np.float32, np.float32, np.int32), texrefs=[self._tex_field])
+        self._get_val_grad_fn.prepare(("P", "P", "P", "P", "P", np.float32, np.float32, np.int32), texrefs=[self._tex_field])
         self._chomp_smooth_val_grad_fn = self._mod.get_function("chomp_smooth_dist_grad")
-        self._chomp_smooth_val_grad_fn.prepare(("P", "P", "P", "P", np.float32, np.float32, np.float32, np.int32),
+        self._chomp_smooth_val_grad_fn.prepare(("P", "P", "P", "P", "P", np.float32, np.float32, np.float32, np.int32),
                                                texrefs=[self._tex_field])
         self._grid = (int(math.ceil(num_pos / 1024.0)), 1)  # there can be at most 1024 threads per block
         self._block = (min(1024, num_pos), 1, 1)
@@ -360,7 +370,7 @@ class CudaStaticPositionsInterpolator(object):
             values, np array of shape (n,) - interpolated values at the given positions
                 If a position is outside of the grid, the closest value is returned
             grad, np array of shape (n, 3) - gradients at the given positions. The gradients are w.r.t
-                the x, y, z axis of the frame positions is defined in.
+                the x, y, z axis of the base frame given during construction (world frame).
             WARNING: both values and grad are reused for subsequent calls. You should copy the data, if
                 you intend to keep it.
         """
@@ -371,8 +381,8 @@ class CudaStaticPositionsInterpolator(object):
         cuda.memcpy_htod(self._gpu_tf_matrix, tf_matrix.flatten()[:12])
         # execute function
         self._get_val_grad_fn.prepared_call(self._grid, self._block, self._gpu_positions,
-                                            self._gpu_values.gpudata, self._gpu_gradients, self._gpu_tf_matrix,
-                                            self._scale, self._delta, self._num_pos)
+                                            self._gpu_values.gpudata, self._gpu_gradients, self._gpu_grad_dirs,
+                                            self._gpu_tf_matrix, self._scale, self._delta, self._num_pos)
         # copy results to CPU
         self._gpu_values.get(self._cpu_values)
         cuda.memcpy_dtoh(self._cpu_gradients, self._gpu_gradients)
@@ -423,7 +433,7 @@ class CudaStaticPositionsInterpolator(object):
             values, np array of shape (n,) - interpolated values at the given positions
                 If a position is outside of the grid, the closest value is returned
             grad, np array of shape (n, 3) - gradients at the given positions. The gradients are w.r.t
-                the x, y, z axis of the frame positions is defined in.
+                the x, y, z axis of the base frame given during construction (world frame).
             WARNING: both values and grad are reused for subsequent calls. You should copy the data, if
                 you intend to keep it.
         """
@@ -433,8 +443,8 @@ class CudaStaticPositionsInterpolator(object):
         # copy matrix to gpu
         cuda.memcpy_htod(self._gpu_tf_matrix, tf_matrix.flatten()[:12])
         # execute function
-        self._chomp_smooth_val_grad_fn.prepared_call(self._grid, self._block, self._gpu_positions,
-                                                     self._gpu_values.gpudata, self._gpu_gradients, self._gpu_tf_matrix,
+        self._chomp_smooth_val_grad_fn.prepared_call(self._grid, self._block, self._gpu_positions, self._gpu_values.gpudata,
+                                                     self._gpu_gradients, self._gpu_grad_dirs, self._gpu_tf_matrix,
                                                      self._scale, self._delta, np.float32(eps), self._num_pos)
         # copy results to CPU
         # cuda.memcpy_dtoh(self._cpu_values, self._gpu_values)

python-src/hfts_grasp_planner/sdf/grid.py
@@ -608,7 +608,7 @@ class VoxelGrid(object):
             return valid_flags, values, gradients
         return valid_flags, gradients
 
-    def get_cell_gradients_pos(self, positions, b_global_frame=True, b_return_values=True):
+    def get_cell_gradients_pos(self, positions, b_global_frame=True, b_return_values=True, b_force_cpu=False):
         """
             Return the gradients and optionally the values at the given positions
             NOTE: This function throws a ValueError if the stored data type is not float.
@@ -628,7 +628,7 @@ class VoxelGrid(object):
         if self._cells.dtype != np.float_:
             raise ValueError("Can not compute gradients for anything else than floats")
         num_points = positions.shape[0]
-        if self._b_use_cuda and num_points > MIN_NUM_POINTS_CUDA:
+        if self._b_use_cuda and num_points > MIN_NUM_POINTS_CUDA and not b_force_cpu:
             return self.get_cell_gradients_pos_cuda(positions, b_global_frame, b_return_values)
         query_width = 7 if b_return_values else 6
         valid_masks = np.empty((query_width, num_points), dtype=bool)

scripts/test_scripts/test_cuda_interpolation.py
@@ -19,16 +19,16 @@ def gen_data():
         for y in range(data.shape[1]):
             for z in range(data.shape[2]):
                 data[x, y, z] = x * 10 + (1 + y) * (1 + z)
-    data = 1.0 / 10.0 * data
-    return data
+    data = 1.0 / 100.0 * data
+    return data.astype(np.float32)
 
 
 def test_normal():
     data = gen_data()
     positions = np.array([[0.1, 0.5, 0.3], [0.5, 1.0, 1.0],
                           [0.5, 0.0, 0.0], [0.0, 0.5, 0.0],
-                          [0.0, 0.0, 0.5], [0.0, 1.0, 1.0]])
-    tf_matrix = transformations.random_rotation_matrix()
+                          [0.0, 0.0, 0.5], [0.0, 1.0, 1.0]], dtype=np.float32)
+    tf_matrix = transformations.random_rotation_matrix().astype(np.float32)
     # tf_matrix = np.eye(4)
     tf_matrix[:3, 3] = [0.2, 0.1, 0.2]
     interp = cuda_grid.CudaInterpolator(data)
@@ -41,6 +41,77 @@ def test_normal():
     print scipy_out
     # print out_values - mapped_pos
     print out_values - scipy_out
+    IPython.embed()
+
+
+def compute_gradients(pos, data_reader, delta):
+    query_pos = pos + np.array([delta, 0.0, 0.0], dtype=np.float32)
+    px = data_reader(query_pos)
+    # px = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    query_pos = pos - np.array([delta, 0.0, 0.0], dtype=np.float32)
+    # mx = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    mx = data_reader(query_pos)
+    query_pos = pos + np.array([0.0, delta, 0.0], dtype=np.float32)
+    # py = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    py = data_reader(query_pos)
+    query_pos = pos - np.array([0.0, delta, 0.0], dtype=np.float32)
+    # my = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    my = data_reader(query_pos)
+    query_pos = pos + np.array([0.0, 0.0, delta], dtype=np.float32)
+    # pz = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    pz = data_reader(query_pos)
+    query_pos = pos - np.array([0.0, 0.0, delta], dtype=np.float32)
+    mz = data_reader(query_pos)
+    # mz = scipy.ndimage.map_coordinates(data, query_pos.transpose() + 1.0, order=1, mode='nearest')
+    xgrad = (px - mx) / (2.0 * delta)
+    ygrad = (py - my) / (2.0 * delta)
+    zgrad = (pz - mz) / (2.0 * delta)
+    return np.array([xgrad, ygrad, zgrad]).transpose()
+
+
+def test_gradients():
+    data = gen_data()
+    positions = np.array([[0.1, 0.5, 0.3], [0.5, 1.0, 1.0],
+                          [0.5, 0.0, 0.0], [0.0, 0.5, 0.0],
+                          [0.0, 0.0, 0.5], [0.0, 1.0, 1.0]], dtype=np.float32)
+    # tf_matrix = transformations.random_rotation_matrix()
+    tf_matrix = np.eye(4, dtype=np.float32)
+    # tf_matrix[:3, 3] = [0.2, 0.1, 0.2]
+    delta = 0.01
+    interp = cuda_grid.CudaInterpolator(data)
+
+    def read_scipy(pos):
+        return scipy.ndimage.map_coordinates(data, pos.transpose() + 1.0, order=1, mode='nearest')
+
+    def read_cuda(pos):
+        return np.array(interp.interpolate(pos))
+
+    scipy_values = read_scipy(positions)
+    cuda_values, cuda_gradients = interp.gradient(positions, delta=delta)
+    copied_val = np.array(cuda_values)
+    cuda_values, cuda_gradients = interp.gradient(positions, delta=delta)
+    print cuda_values - copied_val
+    mapped_pos = np.dot(positions, tf_matrix[:3, :3].transpose()) + tf_matrix[:3, 3]
+    scipy_gradients = compute_gradients(mapped_pos, read_scipy, delta)
+    mixed_gradients = compute_gradients(mapped_pos, read_cuda, delta)
+    # x, y, z = np.where(data > 0)
+    # all_indices = np.array(zip(x, y, z))
+    print cuda_values
+    print scipy_values
+    print cuda_values - scipy_values
+    # gradients
+    print "Cuda gradients"
+    print cuda_gradients
+    print "Scipy gradients"
+    print scipy_gradients
+    print cuda_gradients - scipy_gradients
+    # mixed gradients
+    print "Mixed gradients"
+    print mixed_gradients
+    print "Mixed - scipy gradients"
+    print mixed_gradients - scipy_gradients
+    print "Mixed - cuda gradients"
+    print mixed_gradients - cuda_gradients
     IPython.embed()
 
 
@@ -97,6 +168,7 @@ def test_slices():
 
 
 if __name__ == "__main__":
+    test_gradients()
     # test_normal()
     # test_slices()
-    test_sum()
+    # test_sum()

scripts/test_scripts/test_sdf_gradients.py
@@ -5,12 +5,14 @@ import hfts_grasp_planner.sdf.core as sdf_mod
 import hfts_grasp_planner.sdf.grid as grid_mod
 import hfts_grasp_planner.utils as utils
 import numpy as np
-import mayavi.mlab
+import openravepy as orpy
+# import mayavi.mlab
 import rospy
 import time
 
+ENV_PATH = '/home/joshua/projects/placement_planning/src/hfts_grasp_planner/data/environments/cabinet_high_clutter.xml'
 GDF_PATH = '/home/joshua/projects/placement_planning/src/hfts_grasp_planner/data/sdfs/gradients/placement_exp_1.static.gdf.npy'
-SDF_PATH = '/home/joshua/projects/placement_planning/src/hfts_grasp_planner/data/sdfs/placement_exp_0.sdf.static.sdf'
+SDF_PATH = '/home/joshua/projects/placement_planning/src/hfts_grasp_planner/data/sdfs/cabinet_high_clutter.static.sdf'
 
 
 def show_precomputed():
@@ -75,7 +77,100 @@ def show_distance_field():
     mayavi.mlab.show()
 
 
+def show_distance_field_fixed_pos():
+    sdf = sdf_mod.SDF.load(SDF_PATH)
+    workspace = sdf.get_grid().get_aabb(bWorld=True)
+    num_samples = 50
+    x = np.linspace(workspace[0] + 0.05, workspace[3] - 0.05, num=num_samples)
+    y = np.linspace(workspace[1] + 0.05, workspace[4] - 0.05, num=num_samples)
+    z = np.linspace(workspace[2] + 0.05, workspace[5] - 0.05, num=num_samples)
+    xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
+    query_pos = np.array([xx, yy, zz]).T.reshape(-1, 3)
+    print "Querying positions"
+    interp = sdf.get_cuda_interpolator(query_pos)
+    start = time.time()
+    values, gradients = interp.chomps_smooth_distance(np.eye(4), 0.04)
+    # values, gradients = interp.gradient(np.eye(4))
+    total_time = time.time() - start
+    print "Query took %fs, that is %fs per position" % (total_time, total_time / pow(num_samples, 3))
+    norms = np.linalg.norm(gradients, axis=1)
+    print 'Min gradient norm: %f, max gradient norm: %f' % (np.min(norms), np.max(norms))
+    print "Min value is %f and max value is %f" % (np.min(values), np.max(values))
+    xx_g, yy_g, zz_g = gradients.reshape(num_samples, num_samples, num_samples, 3).T
+    mayavi_vec_f = mayavi.mlab.pipeline.vector_field(xx, yy, zz, xx_g, yy_g, zz_g)
+    mayavi.mlab.pipeline.vector_cut_plane(mayavi_vec_f, plane_orientation="z_axes")
+    # values = values.reshape((num_samples, num_samples, num_samples)).transpose()
+    # mayavi.mlab.volume_slice(values, slice_index=1, plane_orientation="z_axes")
+    # mayavi.mlab.volume_slice(xx, yy, zz, values.reshape(num_samples, num_samples, num_samples).T,
+    #  slice_index=2, plane_orientation="z_axes")
+    mayavi.mlab.show()
+
+
+def compare_gradient_vals():
+    sdf = sdf_mod.SDF.load(SDF_PATH)
+    workspace = sdf.get_grid().get_aabb(bWorld=True)
+    num_samples = 50
+    x = np.linspace(workspace[0] + 0.05, workspace[3] - 0.05, num=num_samples)
+    y = np.linspace(workspace[1] + 0.05, workspace[4] - 0.05, num=num_samples)
+    z = np.linspace(workspace[2] + 0.05, workspace[5] - 0.05, num=num_samples)
+    xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
+    query_pos = np.array([xx, yy, zz]).T.reshape(-1, 3)
+    print "Querying positions"
+    interp = sdf.get_cuda_interpolator(query_pos)
+    start = time.time()
+    # values, gradients = interp.chomps_smooth_distance(np.eye(4), 0.04)
+    cuda_f_values, cuda_f_gradients = interp.gradient(np.eye(4))
+    total_time = time.time() - start
+    print "GPU query took %fs, that is %fs per position" % (total_time, total_time / pow(num_samples, 3))
+    start = time.time()
+    cpu_values, cpu_gradients = sdf.get_distances_grad(query_pos, b_force_cpu=True)
+    cpu_values = cpu_values.astype(np.float32)
+    cpu_gradients = cpu_gradients.astype(np.float32)
+    total_time = time.time() - start
+    print "CPU query took %fs, that is %fs per position" % (total_time, total_time / pow(num_samples, 3))
+    value_errors = np.abs(cpu_values - cuda_f_values)
+    gradient_diffs = np.abs(cpu_gradients - cuda_f_gradients)
+    gradient_errors = np.linalg.norm(cpu_gradients - cuda_f_gradients, axis=1)
+    print "For fixed position cuda:"
+    print "Mean value error is %f, min/max errors are %f, %f" % (
+        np.mean(value_errors), np.min(value_errors), np.max(value_errors))
+    print "Mean gradient error is %f, min/max errors are %f, %f" % (
+        np.mean(gradient_errors), np.min(gradient_errors), np.max(gradient_errors))
+    print "Diffs in gradients. Mean: %s, min: %s, max: %s" % (str(np.mean(gradient_diffs, axis=0)), str(
+        np.min(gradient_diffs, axis=0)), str(np.max(gradient_diffs, axis=0)))
+    print "Largest value error occurs at idx %i and largest gradient error at %i" % (
+        np.argmax(value_errors), np.argmax(gradient_errors))
+    IPython.embed()
+
+
+def show_probe():
+    sdf = sdf_mod.SDF.load(SDF_PATH)
+    grid = sdf.get_grid()
+    env = orpy.Environment()
+    env.Load(ENV_PATH)
+    body = orpy.RaveCreateKinBody(env, '')
+    body.InitFromSpheres(np.array([[0, 0, 0, 0.01]]))
+    body.SetName("PROBE")
+    env.AddKinBody(body)
+    env.SetViewer('qtcoin')
+    tf = np.eye(4)
+    tf[:3, 3] = [0.7, 1.0, 1.0]
+    body.SetTransform(tf)
+
+    def read_grad_val():
+        pos = body.GetTransform()[:3, 3]
+        _, val, grad = grid.get_cell_gradients_pos_cuda(pos)
+        print "Value is %f, gradient is %s" % (val[0], str(grad[0]))
+        end = pos + grad[0]
+        return env.drawarrow(pos, end, linewidth=0.01)
+    read_grad_val()
+    IPython.embed()
+
+
 if __name__ == "__main__":
     rospy.init_node("TestSDFGradients")
     # show_online_computed()
-    show_distance_field()
+    # show_distance_field()
+    # show_distance_field_fixed_pos()
+    # compare_gradient_vals()
+    show_probe()