a1292740863c0d052b88837cdf794f5915da65a1
Joshua Haustein <haustein@kth.se>
2019-02-24 13:18:58 +0100

    Random sampler does not need to descend in the hierarchy all the time

--- python-src/hfts_grasp_planner/placement/goal_sampler/random_sampler.py
+++ python-src/hfts_grasp_planner/placement/goal_sampler/random_sampler.py
@@ -9,14 +9,35 @@ import hfts_grasp_planner.utils as utils
 
 
 class RandomPlacementSampler(plcmnt_interfaces.PlacementGoalSampler):
-    def __init__(self, hierarchy, solution_constructor, validator, objective, manip_names, b_optimize_constraints):
+    def __init__(self, hierarchy, solution_constructor, validator, objective, manip_names, b_go_to_leaf,
+                 b_optimize_constraints, p_descend=0.5):
+        """
+            Create a new RandomPlacementSampler.
+            ---------
+            Arguments
+            ---------
+            hierarchy, interfaces.PlacementHierarchy - hierarchy to sample placements from
+            solution_constructor, interfaces.PlacementSolutionConstructor
+            validator, interfaces.PlacementValidator
+            objective, interfaces.PlacementObjective
+            manip_names, list of string - manipulator names
+            b_go_to_leaf, bool - If True, the random sampler will only sample leaves of the hierarchy, else
+                it descends the hierarchy at a random location and at each depth (that is sampleable) tosses a coin
+                to decide whether to descend further, or construct a solution for that depth.
+            b_optimize_constraints, bool - If True, tell solution constructor to use projection function to increase the
+                chance to hit a valid solution
+            p_descend, float - probability to descend if b_go_to_leaf is False
+
+        """
         self._hierarchy = hierarchy
         self._solution_constructor = solution_constructor
         self._validator = validator
         self._objective = objective
         self._manip_names = manip_names
         self._best_reached_goal = None
+        self._b_go_to_leaf = b_go_to_leaf
         self._boptimize_constraints = b_optimize_constraints
+        self._p_descend = p_descend
 
     def sample(self, num_solutions, max_attempts, b_improve_objective=True):
         """
@@ -43,10 +64,13 @@ class RandomPlacementSampler(plcmnt_interfaces.PlacementGoalSampler):
             # stop if we have sufficient solutions
             if num_found_solutions == num_solutions:
                 break
-            # sample a random key
+            # sample a random base key, i.e. of a node that we can sample
             key = ()
-            child_key = self._hierarchy.get_random_child_key(key)
-            while child_key is not None:
+            while not self._solution_constructor.can_construct_solution(key):
+                key = self._hierarchy.get_random_child_key(key)
+            # from here on descend depending on what was selected during construction
+            child_key = key
+            while child_key is not None and (self._b_go_to_leaf or bool(np.random.binomial(1, self._p_descend))):
                 key = child_key
                 child_key = self._hierarchy.get_random_child_key(key)
             assert(key is not None)
@@ -56,7 +80,6 @@ class RandomPlacementSampler(plcmnt_interfaces.PlacementGoalSampler):
                 solutions[solution.manip.GetName()].append(solution)
                 num_found_solutions += 1
         rospy.logdebug("Random sampler made %i attempts to sample %i solutions" % (num_attempts, num_found_solutions))
-        # TODO should we sort solutions here?
         return solutions, num_found_solutions
 
     def set_reached_goals(self, goals):
