7e15645a92fff7fd59bfc9114bbbb8cae0da2fd5
Joshua Haustein <haustein@kth.se>
2020-10-28 22:24:24 +0100

    generate goals script + test script for mgplanner

---
+++ scripts/mgplanning/generate_yumi_goals.py
@@ -0,0 +1,358 @@
+#! /usr/bin/python
+import IPython
+import sys
+import os
+import yaml
+import rospy
+import time
+import argparse
+import numpy as np
+import cProfile
+import openravepy as orpy
+# import hfts_grasp_planner.placement
+from hfts_grasp_planner.utils import (is_dynamic_body, inverse_transform,
+                                      get_manipulator_links, set_grasp,
+                                      set_body_color, set_body_alpha,
+                                      get_tf_interpolation)
+import hfts_grasp_planner.sdf.grid as grid_module
+import hfts_grasp_planner.ik_solver as ik_module
+import hfts_grasp_planner.sdf.core as sdf_module
+import hfts_grasp_planner.sdf.robot as robot_sdf_module
+import hfts_grasp_planner.sdf.kinbody as kinbody_sdf_module
+import hfts_grasp_planner.placement.afr_placement.placement_orientations as plcmnt_orientations_mod
+import hfts_grasp_planner.placement.afr_placement.placement_regions as plcmnt_regions_mod
+import hfts_grasp_planner.placement.afr_placement.core as afr_placement_mod
+import hfts_grasp_planner.placement.afr_placement.multi_grasp as afr_dmg_mod
+import hfts_grasp_planner.placement.afr_placement.statsrecording as statsrecording
+import hfts_grasp_planner.placement.goal_sampler.random_sampler as rnd_sampler_mod
+import hfts_grasp_planner.placement.goal_sampler.simple_mcts_sampler as simple_mcts_sampler_mod
+import hfts_grasp_planner.placement.anytime_planner as anytime_planner_mod
+import hfts_grasp_planner.placement.clearance as clearance_mod
+import hfts_grasp_planner.placement.objectives as objectives_mod
+from hfts_grasp_planner.sdf.visualization import visualize_occupancy_grid
+
+
+def draw_volume(env, volume):
+    return env.drawbox(0.5 * (volume[0] + volume[1]),
+                       0.5 * (volume[1] - volume[0]),
+                       np.array([0.3, 0.3, 0.3, 0.3]))
+
+
+def resolve_paths(problem_desc, yaml_file):
+    global_yaml = str(yaml_file)
+    if not os.path.isabs(global_yaml):
+        cwd = os.getcwd()
+        global_yaml = cwd + '/' + global_yaml
+    head, _ = os.path.split(global_yaml)
+    for key in [
+            'or_env', 'occ_file', 'sdf_file', 'urdf_file', 'target_obj_file',
+            'grasp_file', 'robot_occtree', 'robot_occgrid',
+            'reachability_path', 'robot_ball_desc', 'dmg_file',
+            'gripper_information'
+    ]:
+        if key in problem_desc:
+            problem_desc[key] = os.path.normpath(head + '/' +
+                                                 problem_desc[key])
+
+
+def load_grasp(problem_desc):
+    if 'grasp_pose' in problem_desc and 'grasp_config' in problem_desc:
+        return
+    assert 'grasp_file' in problem_desc
+    with open(problem_desc['grasp_file']) as grasp_file:
+        grasp_yaml = yaml.load(grasp_file)
+        if 'grasp_id' in problem_desc:
+            grasp_id = problem_desc['grasp_id']
+        else:
+            grasp_id = 0
+        if grasp_id >= len(grasp_yaml):
+            raise IOError("Invalid grasp id: " + str(grasp_id))
+        problem_desc['grasp_pose'] = grasp_yaml[grasp_id]['grasp_pose']
+        # grasp_pose = orpy.matrixFromQuat(problem_desc["grasp_pose"][3:])
+        # grasp_pose[:3, 3] = problem_desc["grasp_pose"][:3]
+        # grasp_pose = utils.inverse_transform(grasp_pose)
+        # problem_desc['grasp_pose'][:3] = grasp_pose[:3, 3]
+        # problem_desc['grasp_pose'][3:] = orpy.quatFromRotationMatrix(grasp_pose)
+        problem_desc['grasp_config'] = grasp_yaml[grasp_id]['grasp_config']
+
+
+def show_solution(sol, target_obj):
+    robot = sol.manip.GetRobot()
+    set_grasp(sol.manip, target_obj, inverse_transform(sol.grasp_tf),
+              sol.grasp_config)
+    robot.SetDOFValues(sol.arm_config, sol.manip.GetArmIndices())
+    env = robot.GetEnv()
+    if env.CheckCollision(robot) or robot.CheckSelfCollision():
+        rospy.logerr("ERROR: The shown solution is in collision!")
+        import IPython
+        IPython.embed()
+
+
+def show_solutions(solutions, target_obj, sleep_time=0.1):
+    for sol in solutions:
+        robot = sol.manip.GetRobot()
+        before_config = robot.GetDOFValues()
+        before_tf = target_obj.GetTransform()
+        show_solution(sol, target_obj)
+        time.sleep(sleep_time)
+        robot.SetDOFValues(before_config)
+        robot.Release(target_obj)
+        target_obj.SetTransform(before_tf)
+
+
+def tf_matrix_to_yaml_dict(m):
+    """Transform a 4x4 transformation matrix to store as yaml.
+
+    Args:
+        m (numpy array of shape (4, 4)): The matrix to store
+    Returns:
+        dict: a dictionary that can be serialized in yaml
+    """
+    return {
+        'pos': map(float, list(m[:3, 3])),
+        'quat': map(float, list(orpy.quatFromRotationMatrix(m[:3, :3])))
+    }
+
+
+def grasp_to_yaml_dict(grasp):
+    """Return a dictionary representation of the given grasp for yaml serializing.
+
+    Args:
+        grasp (placement.afr_placement.multi_grasp.Grasp): The grasp to format as dictionary.
+    Returns:
+        dict: A dictionary storing all relevant information about the given grasp.
+    """
+    return {
+        'id': grasp.gid,
+        'cost': float(grasp.distance),
+        'config': map(float, list(grasp.config)),
+        'oTe': tf_matrix_to_yaml_dict(grasp.oTe)
+    }
+
+
+def goal_to_yaml_dict(goal):
+    """Return a dictionary representation of the given goal to serialize in yaml.
+
+    Args:
+        goal (PlacementGoal): The goal to represent as a dict.
+    Returns:
+        dict: A dictionary representation of the given goal.
+    """
+    return {
+        'arm_config': map(float, list(goal.arm_config)),
+        'grasp_id': goal.grasp_id,
+        'objective_value': float(goal.objective_value)
+    }
+
+
+def store_placements(placements, grasp_set, filename):
+    """Store the given placements in a file.
+
+    Args:
+        placements (list of PlacementGoal): The placement goals to store.
+        grasp_set (DMGGraspSet): The superset of grasps.
+        filename (str): Filename to store placements in.
+    """
+    if placements:
+        # first determine what grasps we have
+        grasp_ids = set([goal.grasp_id for goal in placements])
+        # collect grasp information
+        grasps = [grasp_set.get_grasp(gid) for gid in grasp_ids]
+        # store everything in a dict and save to file
+        output_dict = {}
+        output_dict['manipulator'] = str(placements[0].manip.GetName())
+        output_dict['grasps'] = [grasp_to_yaml_dict(grasp) for grasp in grasps]
+        output_dict['goals'] = [goal_to_yaml_dict(goal) for goal in placements]
+        with open(filename, 'w') as output_file:
+            yaml.dump(output_dict, output_file)
+
+
+if __name__ == "__main__":
+    # NOTE If the OpenRAVE viewer is created too early, nothing works! Collision checks may be incorrect!
+    parser = argparse.ArgumentParser(
+        "Script to generate a collection of placements in a given scene.")
+    parser.add_argument(
+        'problem_desc',
+        help="Path to a yaml file specifying what world, robot to use etc.",
+        type=str)
+    parser.add_argument('num_goals',
+                        help="Number of goals to sample",
+                        type=int)
+    parser.add_argument('output_path',
+                        help="Filename in which to store generated goals.",
+                        type=str)
+    args = parser.parse_args()
+    with open(args.problem_desc, 'r') as f:
+        problem_desc = yaml.load(f)
+        resolve_paths(problem_desc, args.problem_desc)
+        load_grasp(problem_desc)
+    try:
+        env = orpy.Environment()
+        env.Load(problem_desc['or_env'])
+        # load target object
+        btarget_found = env.Load(problem_desc['target_obj_file'])
+        if not btarget_found:
+            raise ValueError("Could not load target object. Aborting")
+        # ensure the object has a useful name
+        target_obj_name = "target_object"
+        env.GetBodies()[-1].SetName(target_obj_name)
+        # load occupancy grid
+        scene_occ = None
+        try:
+            scene_occ = grid_module.VoxelGrid.load(problem_desc['occ_file'])
+        except IOError as e:
+            rospy.logerr(
+                "Could not load %s. Please provide an occupancy grid of the scene."
+                % problem_desc['occ_file'])
+            rospy.logerr("There is a script to create one!")
+            sys.exit(0)
+        placement_volume = (np.array(problem_desc['plcmnt_volume'][:3]),
+                            np.array(problem_desc['plcmnt_volume'][3:]))
+        occ_target_volume = scene_occ.get_subset(placement_volume[0],
+                                                 placement_volume[1])
+        # create scene sdf
+        dynamic_bodies = [
+            body for body in env.GetBodies() if is_dynamic_body(body)
+        ]
+        scene_sdf = sdf_module.SceneSDF(env, [],
+                                        excluded_bodies=dynamic_bodies)
+        if os.path.exists(problem_desc['sdf_file']):
+            now = time.time()
+            scene_sdf.load(problem_desc['sdf_file'])
+            rospy.logdebug("Loading scene sdf took %fs" % (time.time() - now))
+        else:
+            rospy.logerr(
+                "Could not load %s. Please provide a signed distance field of the scene."
+                % problem_desc['sdf_file'])
+            rospy.logerr("There is a script to create one!")
+            sys.exit(0)
+        # extract placement orientations
+        target_object = env.GetKinBody(target_obj_name)
+        orientations = plcmnt_orientations_mod.compute_placement_orientations(
+            target_object)
+        # extract placement regions
+        gpu_kit = plcmnt_regions_mod.PlanarRegionExtractor()
+        surface_grid, labels, num_regions, regions = gpu_kit.extract_planar_regions(
+            occ_target_volume, max_region_size=0.3)
+        if num_regions == 0:
+            print "No placement regions found"
+            sys.exit(0)
+        # compute surface distance field
+        obj_radius = np.linalg.norm(target_object.ComputeAABB().extents())
+        global_region_info = plcmnt_regions_mod.PlanarRegionExtractor.compute_surface_distance_field(
+            surface_grid, 2.0 * obj_radius)
+        # prepare robot data
+        robot = env.GetRobot(problem_desc['robot_name'])
+        # set initial grasp and create grasp set
+        # grasp_pose is oTe
+        manip = robot.GetActiveManipulator()
+        grasp_pose = orpy.matrixFromQuat(problem_desc["grasp_pose"][3:])
+        grasp_pose[:3, 3] = problem_desc["grasp_pose"][:3]
+        set_grasp(manip, target_object, inverse_transform(grasp_pose),
+                  problem_desc['grasp_config'])
+        gripper_info = afr_dmg_mod.load_gripper_info(
+            problem_desc['gripper_information'], manip.GetName())
+        grasp_set = afr_dmg_mod.DMGGraspSet(manip, target_object,
+                                            problem_desc['target_obj_file'],
+                                            gripper_info,
+                                            problem_desc['dmg_file'])
+        # prepare manipulator data
+        link_names = []
+        if 'manipulator' in problem_desc:
+            robot.SetActiveManipulator(problem_desc['manipulator'])
+        ik_solver = ik_module.IKSolver(manip, problem_desc['urdf_file'])
+        manip_data = {}
+        manip_data[manip.GetName(
+        )] = afr_dmg_mod.MultiGraspAFRRobotBridge.ManipulatorData(
+            manip, ik_solver, grasp_set, gripper_info['gripper_file'])
+        manip_links = [link.GetName() for link in get_manipulator_links(manip)]
+        # remove base link - it does not move so
+        manip_links.remove(manip.GetBase().GetName())
+        link_names.extend(manip_links)
+        # load robot ball approximation
+        robot_ball_approx = robot_sdf_module.RobotBallApproximation(
+            robot, problem_desc['robot_ball_desc'])
+        # build robot_octree
+        try:
+            now = time.time()
+            robot_occgrid = robot_sdf_module.RobotOccupancyGrid.load(
+                base_file_name=problem_desc['robot_occgrid'],
+                robot=robot,
+                link_names=link_names)
+            rospy.logdebug("Loading robot occgrid took %fs" %
+                           (time.time() - now))
+        except IOError:
+            now = time.time()
+            robot_occgrid = robot_sdf_module.RobotOccupancyGrid(
+                problem_desc['parameters']['occ_tree_cell_size'],
+                robot=robot,
+                link_names=link_names)
+            robot_occgrid.save(problem_desc['robot_occgrid'])
+            rospy.logdebug("Creating robot occgrid took %fs" %
+                           (time.time() - now))
+        # prepare robot data
+        urdf_content = None
+        with open(problem_desc['urdf_file'], 'r') as urdf_file:
+            urdf_content = urdf_file.read()
+        robot_data = afr_placement_mod.AFRRobotBridge.RobotData(
+            robot, robot_occgrid, manip_data, urdf_content, robot_ball_approx)
+        # create object data
+        obj_occgrid = kinbody_sdf_module.RigidBodyOccupancyGrid(
+            problem_desc['parameters']['occ_tree_cell_size'],
+            target_object.GetLinks()[0])
+        obj_occgrid.setup_cuda_sdf_access(scene_sdf)
+        object_data = afr_placement_mod.AFRRobotBridge.ObjectData(
+            target_object, obj_occgrid)
+        # create objective function
+        now = time.time()
+        if 'objective_fn' in problem_desc:
+            if problem_desc['objective_fn'] == 'minimize_clearance':
+                obj_fn = clearance_mod.ClearanceObjective(occ_target_volume,
+                                                          obj_occgrid,
+                                                          b_max=False)
+            elif problem_desc['objective_fn'] == 'maximize_clearance':
+                obj_fn = clearance_mod.ClearanceObjective(occ_target_volume,
+                                                          obj_occgrid,
+                                                          b_max=True)
+            elif problem_desc['objective_fn'] == 'deep_shelf':
+                obj_fn = objectives_mod.DeepShelfObjective(target_object,
+                                                           occ_target_volume,
+                                                           obj_occgrid,
+                                                           b_max=True)
+        else:
+            obj_fn = clearance_mod.ClearanceObjective(occ_target_volume,
+                                                      obj_occgrid,
+                                                      b_max=False)
+        # create afr hierarchy
+        hierarchy = afr_placement_mod.AFRHierarchy([manip],
+                                                   regions,
+                                                   orientations,
+                                                   so2_depth=4,
+                                                   so2_branching=4)
+        # create afr bridge
+        afr_bridge = afr_dmg_mod.MultiGraspAFRRobotBridge(
+            afr_hierarchy=hierarchy,
+            robot_data=robot_data,
+            object_data=object_data,
+            objective_fn=obj_fn,
+            global_region_info=global_region_info,
+            scene_sdf=scene_sdf,
+            parameters=problem_desc['parameters'])
+        # create goal sampler
+        goal_sampler = simple_mcts_sampler_mod.SimpleMCTSPlacementSampler(
+            hierarchy,
+            afr_bridge,
+            afr_bridge,
+            afr_bridge, [manip.GetName()],
+            debug_visualizer=None,
+            parameters=problem_desc["parameters"])
+
+        solutions, num_solutions = goal_sampler.sample(args.num_goals, 1000000)
+        if num_solutions < args.num_goals:
+            print "Sorry, only found %i solutions (%i queried)" % (
+                num_solutions, args.num_goals)
+        store_placements(solutions[manip.GetName()], grasp_set,
+                         args.output_path)
+        IPython.embed()
+    finally:
+        orpy.RaveDestroy()
---
+++ scripts/mgplanning/run_mgplanner_yumi.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+import os
+import time
+import yaml
+import argparse
+import random
+import numpy as np
+import openravepy as orpy
+import IPython
+from hfts_grasp_planner.placement.anytime_planner import MGMotionPlanner
+from hfts_grasp_planner.placement.goal_sampler.interfaces import PlacementGoalSampler
+import hfts_grasp_planner.utils as hfts_utils
+
+# def sample_arm_configs(manip, num):
+#     robot = manip.GetRobot()
+#     env = robot.GetEnv()
+#     with robot:
+#         robot.SetActiveDOFs(manip.GetArmIndices())
+#         lower, upper = robot.GetActiveDOFLimits()
+#         joint_range = upper - lower
+#         samples = []
+#         while len(samples) < num:
+#             config = np.random.random(manip.GetArmDOF())
+#             config = config * joint_range + lower
+#             robot.SetActiveDOFValues(config)
+#             if not env.CheckCollision(
+#                     robot) and not robot.CheckSelfCollision():
+#                 samples.append(config)
+#     return samples
+
+
+def show_goals(goals, manip, target_object):
+    robot = manip.GetRobot()
+    robot.SetActiveDOFs(manip.GetArmIndices())
+    for goal in goals:
+        robot.SetActiveDOFValues(goal.arm_config)
+        hfts_utils.set_grasp(manip, target_object,
+                             hfts_utils.inverse_transform(goal.grasp_tf),
+                             goal.grasp_config)
+        time.sleep(1.3)
+
+
+def time_traj(robot, traj, vel_scale=0.4):
+    """
+        Retime the given trajectory.
+    """
+    with robot:
+        vel_limits = robot.GetDOFVelocityLimits()
+        robot.SetDOFVelocityLimits(vel_scale * vel_limits)
+        orpy.planningutils.RetimeTrajectory(traj, hastimestamps=False)
+        robot.SetDOFVelocityLimits(vel_limits)
+    return traj
+
+
+def show_traj(robot, target_object, traj, goal):
+    # execute traj
+    hfts_utils.set_grasp(goal.manip, target_object,
+                         hfts_utils.inverse_transform(goal.grasp_tf),
+                         goal.grasp_config)
+    robot.GetController().SetPath(traj)
+    robot.WaitForController(0)
+
+
+def yaml_dict_to_tf_matrix(adict):
+    """Convert a yaml representation of a tf matrix into a numpy tf matrix.
+
+    Args:
+        adict (dict): A dictionary with two items 'pos': [px, py, pz] and 'quat': [qx, qy, qz, qw]
+            where 'pos' is the position and 'quat' the rotation of the tf.
+    Returns:
+        tf, np.array of shape (4, 4)
+    """
+    pos = np.array(adict['pos'])
+    quat = np.array(adict['quat'])
+    m = orpy.matrixFromQuat(quat)
+    m[:3, 3] = pos
+    return m
+
+
+def load_goals(robot, target_object, filename):
+    """Load goals from the given file.
+
+    Args:
+        robot (OpenRAVE Robot): robot to load goals for
+        target_object (OpenRAVE KinBody): the target object
+        filename (str): Filename to load goals from
+    Returns:
+        list of PlacementGoals
+    """
+    with open(filename, 'r') as goal_file:
+        goal_data = yaml.load(goal_file)
+    # first create a map from grasp id to grasp information
+    grasp_infos = {}
+    for grasp in goal_data['grasps']:
+        grasp_infos[grasp['id']] = {
+            'config': np.array(grasp['config']),
+            'oTe': yaml_dict_to_tf_matrix(grasp['oTe']),
+            'cost': grasp['cost'],
+        }
+    # now create the list of goals
+    manip = robot.GetManipulator(goal_data['manipulator'])
+    if manip is None:
+        raise ValueError("Could not retrieve manipulator %s" %
+                         goal_data['manipulator'])
+    goals = []
+    for goal in goal_data['goals']:
+        gid = goal['grasp_id']
+        grasp_info = grasp_infos[gid]
+        # set grasp to compute object tf
+        with robot.CreateKinBodyStateSaver():
+            hfts_utils.set_grasp(
+                manip, target_object,
+                hfts_utils.inverse_transform(grasp_info['oTe']),
+                grasp_info['config'])
+            obj_tf = target_object.GetTransform()
+        # create actual goal
+        goals.append(
+            PlacementGoalSampler.PlacementGoal(
+                manip=manip,
+                arm_config=goal['arm_config'],
+                obj_tf=obj_tf,
+                key=len(goals),
+                objective_value=goal['objective_value'],
+                grasp_tf=grasp_info['oTe'],
+                grasp_config=grasp_info['config'],
+                grasp_id=gid))
+    return goals
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Script to run and test a multi-grasp motion planner on Yumi.")
+    parser.add_argument('env_file',
+                        help="Path to a OpenRAVE xml specifying the scene",
+                        type=str)
+    parser.add_argument(
+        'target_obj_file',
+        help='Path to an OpenRAVE xml describing the target Kinbody.',
+        type=str)
+    parser.add_argument('goal_file',
+                        help="Path to a yaml file containing goals",
+                        type=str)
+    parser.add_argument('algorithm_name',
+                        help="Name of the algorithm to use",
+                        type=str)
+    parser.add_argument('graph_name',
+                        help="Name of the graph to use",
+                        type=str)
+    args = parser.parse_args()
+
+    # base_path = os.path.dirname(__file__)
+    # env_file = os.path.normpath(
+    #     base_path + '/../../models/environments/placement_exp_0.xml')
+    env = orpy.Environment()
+    orpy.RaveSetDebugLevel(orpy.DebugLevel.Debug)
+    # load scene and target
+    env.Load(args.env_file)
+    if not env.Load(args.target_obj_file):
+        raise ValueError("Could not load target object. Aborting")
+    # ensure the object has a useful name
+    target_obj_name = "target_object"
+    target_object = env.GetBodies()[-1]  # object that was loaded last
+    target_object.SetName(target_obj_name)
+    robot = env.GetRobots()[0]
+    # load goals
+    goals = load_goals(robot, target_object, args.goal_file)
+    # goals = [goal for goal in goals if goal.objective_value > 0.7]
+    manip = goals[0].manip
+    robot.SetActiveDOFs(manip.GetArmIndices())
+    start_config = robot.GetDOFValues()
+    # create planner
+    planner = MGMotionPlanner("%s;%s" % (args.algorithm_name, args.graph_name),
+                              manip)
+    planner.setup(target_object)
+    planner.addGoals(goals)
+    # plan
+    trajectories, reached_goals = planner.plan(10.0)
+    trajectories = [time_traj(robot, traj) for traj in trajectories]
+    env.SetViewer('qtcoin')
+    IPython.embed()
