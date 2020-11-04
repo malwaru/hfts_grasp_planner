d8fa522a42eb88d7e6c88514eaac98abdf2df6e9
Joshua Haustein <haustein@kth.se>
2020-11-04 21:29:18 +0100

    Publicly inherit MultiGraspMP

--- include/hfts_grasp_planner/placement/mp/ImgGraphSearch.h
+++ include/hfts_grasp_planner/placement/mp/ImgGraphSearch.h
@@ -5,24 +5,27 @@
 #include <set>
 #include <vector>
 
-namespace placement {
-namespace mp {
-    class ImgGraphSearch : MultiGraspMP {
-    public:
-        // A wrapper around MGGraphSearchMP for 2d image-based state spaces that implements the MultiGraspMP interface
-        ImgGraphSearch(const std::experimental::filesystem::path& root_path,
-            const Config& start, const mgsearch::MGGraphSearchMP::Parameters& params);
-        ~ImgGraphSearch();
+namespace placement
+{
+namespace mp
+{
+class ImgGraphSearch : public MultiGraspMP
+{
+public:
+  // A wrapper around MGGraphSearchMP for 2d image-based state spaces that implements the MultiGraspMP interface
+  ImgGraphSearch(const std::experimental::filesystem::path& root_path, const Config& start,
+                 const mgsearch::MGGraphSearchMP::Parameters& params);
+  ~ImgGraphSearch();
 
-        void plan(std::vector<Solution>& new_paths, double time_limit) override;
-        void pausePlanning() override;
-        void addGrasp(const Grasp& grasp) override;
-        void addGoal(const Goal& goal) override;
-        void removeGoals(const std::vector<unsigned int>& goal_ids) override;
+  void plan(std::vector<Solution>& new_paths, double time_limit) override;
+  void pausePlanning() override;
+  void addGrasp(const Grasp& grasp) override;
+  void addGoal(const Goal& goal) override;
+  void removeGoals(const std::vector<unsigned int>& goal_ids) override;
 
-    private:
-        mgsearch::MGGraphSearchMPPtr _planner;
-        mgsearch::ImageStateSpacePtr _state_space;
-    };
-}
-}
+private:
+  mgsearch::MGGraphSearchMPPtr _planner;
+  mgsearch::ImageStateSpacePtr _state_space;
+};
+}  // namespace mp
+}  // namespace placement
